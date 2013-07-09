73db44a77be0e629940c3d8bb727c1522b4f46ab
Mathieu Faverge <mathieu.faverge@inria.fr>
2013-07-09 17:37:31 +0200

    Clean order check

--- pastix/order/order_check.c
+++ pastix/order/order_check.c
@@ -1,115 +1,91 @@
-/* Copyright INRIA 2004
-**
-** This file is part of the Scotch distribution.
-**
-** The Scotch distribution is libre/free software; you can
-** redistribute it and/or modify it under the terms of the
-** GNU Lesser General Public License as published by the
-** Free Software Foundation; either version 2.1 of the
-** License, or (at your option) any later version.
-**
-** The Scotch distribution is distributed in the hope that
-** it will be useful, but WITHOUT ANY WARRANTY; without even
-** the implied warranty of MERCHANTABILITY or FITNESS FOR A
-** PARTICULAR PURPOSE. See the GNU Lesser General Public
-** License for more details.
-**
-** You should have received a copy of the GNU Lesser General
-** Public License along with the Scotch distribution; if not,
-** write to the Free Software Foundation, Inc.,
-** 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-**
-** $Id: order_check.c 2 2004-06-02 14:05:03Z ramet $
-*/
-/************************************************************/
-/**                                                        **/
-/**   NAME       : order_check.c                           **/
-/**                                                        **/
-/**   AUTHORS    : Francois PELLEGRINI                     **/
-/**                                                        **/
-/**   FUNCTION   : Part of a parallel direct block solver. **/
-/**                This module checks the consistency of   **/
-/**                orderings.                              **/
-/**                                                        **/
-/**   DATES      : # Version 0.0  : from : 06 oct 1998     **/
-/**                                 to     06 oct 1998     **/
-/**                # Version 1.0  : from : 19 nov 2003     **/
-/**                                 to     20 nov 2003     **/
-/**                # Version 2.0  : from : 28 feb 2004     **/
-/**                                 to     28 feb 2004     **/
-/**                                                        **/
-/************************************************************/
-
-/*
-**  The defines and includes.
-*/
-
-#define ORDER_CHECK
-
+/**
+ *
+ * @file order_check.c
+ *
+ *  PaStiX order routines
+ *  PaStiX is a software package provided by Inria Bordeaux - Sud-Ouest,
+ *  LaBRI, University of Bordeaux 1 and IPB.
+ *
+ * Contains function to check correctness of a ordering structure.
+ *
+ * @version 5.1.0
+ * @author Francois Pellegrini
+ * @date 2013-06-24
+ *
+ **/
 #include "common.h"
 #include "order.h"
 
-/***********************************/
-/*                                 */
-/* The ordering handling routines. */
-/*                                 */
-/***********************************/
-
-/*+ This routine checks the consistency
-*** of the given ordering.
-*** It returns:
-*** - 0   : on success.
-*** - !0  : on error.
-+*/
-
+/**
+ *******************************************************************************
+ *
+ * @ingroup pastix_ordering_internal
+ *
+ * orderCheck - This routine checks the correctness of the ordering stucture.
+ *
+ *******************************************************************************
+ *
+ * @param[in] ordeptr
+ *          The ordering structure to check.
+ *
+ *******************************************************************************
+ *
+ * @return
+ *          \retval PASTIX_SUCESS on successful exit.
+ *          \retval PASTIX_ERR_BADPARAMETER if the ordering structure is incorrect.
+ *
+ *******************************************************************************/
 int
-orderCheck (
-const Order * restrict const  ordeptr)
+orderCheck (const Order * const  ordeptr)
 {
-  pastix_int_t                   baseval;                  /* Node base value            */
-  pastix_int_t                   vnodmax;                  /* Maximum node value         */
-  pastix_int_t                   vnodnum;                  /* Number of current node     */
-  pastix_int_t                   rangnum;                  /* Current column block index */
-  const pastix_int_t * restrict  peritax;                  /* Based access to peritab    */
-  const pastix_int_t * restrict  permtax;                  /* Based access to permtab    */
+    pastix_int_t         baseval;                  /* Node base value            */
+    pastix_int_t         vnodmax;                  /* Maximum node value         */
+    pastix_int_t         vnodnum;                  /* Number of current node     */
+    pastix_int_t         rangnum;                  /* Current column block index */
+    const pastix_int_t * peritax;                  /* Based access to peritab    */
+    const pastix_int_t * permtax;                  /* Based access to permtab    */
 
-  if (ordeptr->cblknbr < 0) {
-    errorPrint ("orderCheck: invalid nunber of column blocks");
-    return     (1);
-  }
-
-  baseval = ordeptr->rangtab[0];                  /* Get base value */
-  if (baseval < 0) {
-    errorPrint ("orderCheck: invalid vertex node base number");
-    return     (1);
-  }
-
-  peritax = ordeptr->peritab - baseval;           /* Set based accesses */
-  vnodmax = ordeptr->rangtab[ordeptr->cblknbr] - 1;
-
-  for (rangnum = 0; rangnum < ordeptr->cblknbr; rangnum ++) {
-    if ((ordeptr->rangtab[rangnum] <  baseval) ||
-        (ordeptr->rangtab[rangnum] >  vnodmax) ||
-        (ordeptr->rangtab[rangnum] >= ordeptr->rangtab[rangnum + 1])) {
-      errorPrint ("orderCheck: invalid range array");
-      return     (1);
+    if (ordeptr->cblknbr < 0) {
+        errorPrint ("orderCheck: invalid nunber of column blocks");
+        return PASTIX_ERR_BADPARAMETER;
     }
-  }
 
-  permtax = ordeptr->permtab - baseval;
-
-  for (vnodnum = baseval;
-       vnodnum <= vnodmax; vnodnum ++) {
-    pastix_int_t                   vnodold;
-
-    vnodold = peritax[vnodnum];
-    if ((vnodold < baseval) ||
-        (vnodold > vnodmax) ||
-        (permtax[vnodold] != vnodnum)) {
-      errorPrint ("orderCheck: invalid permutation arrays");
-      return     (1);
+    baseval = ordeptr->baseval;                  /* Get base value */
+    if (baseval < 0) {
+        errorPrint ("orderCheck: invalid vertex node base number");
+        return PASTIX_ERR_BADPARAMETER;
     }
-  }
 
-  return (0);
+    assert(baseval == ordeptr->rangtab[0]);
+
+    peritax = ordeptr->peritab - baseval;           /* Set based accesses */
+    vnodmax = ordeptr->rangtab[ordeptr->cblknbr] - 1;
+
+    assert(vnodmax == ordeptr->vertnbr);
+
+    for (rangnum = 0; rangnum < ordeptr->cblknbr; rangnum ++) {
+        if ((ordeptr->rangtab[rangnum] <  baseval) ||
+            (ordeptr->rangtab[rangnum] >  vnodmax) ||
+            (ordeptr->rangtab[rangnum] >= ordeptr->rangtab[rangnum + 1])) {
+            errorPrint ("orderCheck: invalid range array");
+            return PASTIX_ERR_BADPARAMETER;
+        }
+    }
+
+    permtax = ordeptr->permtab - baseval;
+
+    for (vnodnum = baseval;
+         vnodnum <= vnodmax; vnodnum ++) {
+        pastix_int_t                   vnodold;
+
+        vnodold = peritax[vnodnum];
+        if ((vnodold < baseval) ||
+            (vnodold > vnodmax) ||
+            (permtax[vnodold] != vnodnum)) {
+            errorPrint ("orderCheck: invalid permutation arrays");
+            return PASTIX_ERR_BADPARAMETER;
+        }
+    }
+
+    return PASTIX_SUCESS;
 }
