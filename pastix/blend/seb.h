c00282c20e8101487efa43704e318c6f581c9de8
Mathieu Faverge <mathieu.faverge@inria.fr>
2013-09-25 15:44:22 +0200

    Remove seb.h

--- pastix/blend/seb.h
+++
@@ -1,3 +0,0 @@
-
-void tree2graph(const EliminTree * etree, const CostMatrix *costmtx, SCOTCH_Graph *graph);
-void SetCandtab(Cand *candtab, const pastix_int_t *parttab, const pastix_int_t vertnbr);
