1a5cfcda37c0890ab620c074cab8fb279a1f6ae6
Mathieu Faverge <mathieu.faverge@inria.fr>
2013-10-29 11:19:19 +0100

    Remove old part of code that was dealing with dense and 2D distribution

--- pastix/blend/task.c
+++ pastix/blend/task.c
@@ -26,37 +26,25 @@
 void taskBuild(SimuCtrl *simuctrl, SymbolMatrix *symbptr, Cand *candtab,
                const Dof * dofptr, BlendCtrl *ctrl)
 {
-    pastix_int_t i, j, k;
+    pastix_int_t i, j;
     pastix_int_t tasknbr = 0;
-    pastix_int_t odb_nbr;
-    pastix_int_t L, h, g;
-    pastix_int_t firstE1task, firstE2task;
-
     SimuTask *task = NULL;
-    /** Count number of task **/
+
+    /* Count number of task */
     for(i=0;i<symbptr->cblknbr;i++)
     {
-        switch(candtab[i].distrib)
+        switch(candtab[i].cblktype)
         {
-        case D1 :
-            /* Task 1D */
+        case CBLK_1D:
+        case CBLK_SPLIT:
             tasknbr++;
             break;
-        case D2 :
-            /* number of extra diagonal blocks in the cblk */
-            odb_nbr = symbptr->cblktab[i+1].bloknum - symbptr->cblktab[i].bloknum-1;
-            tasknbr += 1 + odb_nbr + (odb_nbr*(odb_nbr+1))/2;
-            break;
-        case DENSE :
-            fprintf(stderr, "DENSE cblk \n");
-            tasknbr++;
-            goto tnext;
         default:
             fprintf(stderr, "Task No %ld has wrong type \n", (long)i);
             EXIT(MOD_BLEND,INTERNAL_ERR);
         }
     }
-  tnext:
+
     simuctrl->tasknbr = tasknbr;
     /*fprintf(stderr, "TASKNBR %ld \n" , tasknbr);
      EXIT(MOD_BLEND,INTERNAL_ERR);*/
@@ -68,9 +56,10 @@ void taskBuild(SimuCtrl *simuctrl, SymbolMatrix *symbptr, Cand *candtab,
     tasknbr = 0;
     for(i=0;i<symbptr->cblknbr;i++)
     {
-        switch(candtab[i].distrib)
+        switch(candtab[i].cblktype)
         {
-        case D1:
+        case CBLK_1D:
+        case CBLK_SPLIT:
             task->taskid   = COMP_1D;
             task->prionum  = -1;
             task->cblknum  = i;
@@ -89,123 +78,12 @@ void taskBuild(SimuCtrl *simuctrl, SymbolMatrix *symbptr, Cand *candtab,
             task++;
             break;
 
-
-        case D2:
-            L  = symbptr->cblktab[i].lcolnum - symbptr->cblktab[i].fcolnum + 1;
-            L *= (dofptr)->noddval;
-
-            /** Factorize diagonal block task**/
-            task->taskid   = DIAG;
-            task->prionum  = -1;
-            task->cblknum  = i;
-            task->bloknum  = symbptr->cblktab[i].bloknum;
-            task->bloknum2 = -1;
-            task->ftgtcnt  = 0;
-            task->ctrbcnt  = 0;
-            task->facebloknum = -1;
-            /* set task indice in bloktab */
-            simuctrl->bloktab[task->bloknum].tasknum = tasknbr;
-            task->cost = (double)DIAGCost(L);
-            /*fprintf(stderr, "DIAGCost %f dost %f cost %f \n", DIAGCost(L), dost, task->cost);*/
-            /*fprintf(stderr, "cost %f copy %g PPF %g \n", dost, PERF_COPY(L),  PERF_PPF(L) );*/
-
-            timerSet(&(task->time), 0.0);
-            /* OIMBE il faut prendre en compte toute la taille du message (pareil pour les fan in targets */
-            /*task->timesend =TIME_STARTUP + TIME_BANDWIDTH * L*L*sizeof(double);*/
-            task->mesglen = L*L*sizeof(double);
-            task->tasknext = -1;
-            tasknbr++;
-            task++;
-            firstE1task = tasknbr;
-            for(j=symbptr->cblktab[i].bloknum+1;j<symbptr->cblktab[i+1].bloknum;j++)
-            {
-                h  = symbptr->bloktab[j].lrownum - symbptr->bloktab[j].frownum + 1;
-                h *= (dofptr)->noddval;
-                /** Solxve odb task **/
-                task->taskid   = E1;
-                task->prionum  = -1;
-                task->cblknum  = i;
-                task->bloknum  = j;
-                task->bloknum2 = symbptr->cblktab[i].bloknum;
-                task->ftgtcnt  = 0;
-                task->ctrbcnt  = 0;
-                task->facebloknum = -1;
-                if(j<symbptr->cblktab[i+1].bloknum - 1)
-                    task->tasknext = tasknbr + symbptr->cblktab[i+1].bloknum-j+1;
-                else
-                {
-                    /*task->tasknext = -1;*/
-                    /** Now we make a cycle **/
-                    task->tasknext = firstE1task;
-                }
-
-                /* set task indice in bloktab */
-                simuctrl->bloktab[j].tasknum = tasknbr;
-                task->cost = E1Cost(L, h);
-                timerSet(&(task->time), 0.0);
-                /*task->timesend = TIME_STARTUP + TIME_BANDWIDTH * L*h * sizeof(double);*/
-                task->mesglen = L*h * sizeof(double);
-                tasknbr++;
-                task++;
-                firstE2task = tasknbr;
-                {
-                    pastix_int_t lastbloknum;
-                    lastbloknum = 0;
-                    for(k=j;k<symbptr->cblktab[i+1].bloknum;k++)
-                    {
-                        g = symbptr->bloktab[k].lrownum - symbptr->bloktab[k].frownum + 1;
-                        g *= (dofptr)->noddval;
-                        /** Compute contrib task **/
-                        task->taskid = E2;
-                        task->prionum = -1;
-                        task->cblknum = i;
-                        task->bloknum = k;
-                        task->ftgtcnt = 0;
-                        task->ctrbcnt = 0;
-                        task->bloknum2 = j;
-                        task->facebloknum = getFaceBlockE2(lastbloknum, j, k, symbptr, ctrl->ricar);
-                        lastbloknum = task->facebloknum;
-                        task->cost = E2Cost(L, h, g);
-                        timerSet(&(task->time), 0.0);
-                        task->mesglen =  0.0;
-                        if(k<symbptr->cblktab[i+1].bloknum - 1)
-                            task->tasknext = tasknbr+1;
-                        else
-                        {
-                            /*task->tasknext = -1;*/
-                            /** We chain the E2 in a cycle now **/
-                            task->tasknext = firstE2task;
-                        }
-                        tasknbr++;
-                        task++;
-                    }
-                }
-            }
-            break;
-
-        case DENSE:
-            task->taskid = DRUNK;
-            task->prionum = -1;
-            task->cblknum = i;
-            task->bloknum = symbptr->cblktab[i].bloknum;
-            task->bloknum2 = (symbptr->nodenbr - symbptr->cblktab[i].fcolnum)*dofptr->noddval;
-            task->ctrbcnt = 0;
-            task->facebloknum = -1;
-            task->cost = -1;
-            timerSet(&(task->time), 0.0);
-            task->mesglen = 0.0;
-            task->tasknext = -1;
-            for(j=symbptr->cblktab[i].bloknum;j<symbptr->bloknbr;j++)
-                simuctrl->bloktab[j].tasknum = tasknbr;
-            tasknbr++;
-            task++;
-            goto tend;
         default:
             fprintf(stderr, "Task No %ld has wrong type \n", (long)i);
             EXIT(MOD_BLEND,INTERNAL_ERR);
         }
     }
-  tend:
+
 #ifdef DEBUG_BLEND
     ASSERT(simuctrl->tasknbr == tasknbr,MOD_BLEND);
     for(i=0;i<simuctrl->tasknbr;i++)
