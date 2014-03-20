cca003be55ca31f2d600eb1b36ab7f54b87d5109
Mathieu Faverge <mathieu.faverge@inria.fr>
2014-03-20 12:26:42 +0100

    Start remove the 2D tasks code

--- pastix/sopalin/contrib.c
+++ pastix/sopalin/contrib.c
@@ -1,16 +1,6 @@
 /* Attentes des contributions locale et MPI */
 
-#ifdef RECV_FANIN_OR_BLOCK
-#define RECV_ONE_FANIN API_CALL(recv_waitone_fob)(sopalin_data, me)
-#define RECV_ONE_BLOCK API_CALL(recv_waitone_fob)(sopalin_data, me)
-#else
 #define RECV_ONE_FANIN API_CALL(recv_waitone_fanin)(sopalin_data, me, TASK_PRIONUM(i))
-#define RECV_ONE_BLOCK API_CALL(recv_waitone_block)(sopalin_data, me, TASK_PRIONUM(i))
-#endif
-
-#ifndef X_ARCHi686_pc_linux
-#define inline
-#endif
 
 static inline void API_CALL(wait_contrib_comp_1d)(Sopalin_Data_t *sopalin_data, pastix_int_t me, pastix_int_t i){
 
@@ -54,64 +44,4 @@ static inline void API_CALL(wait_contrib_comp_1d)(Sopalin_Data_t *sopalin_data, 
     COND_WAIT(&(sopalin_data->cond_task[i]), &(sopalin_data->mutex_task[i]));
   }
   MUTEX_UNLOCK(&(sopalin_data->mutex_task[i]));
-
-}
-
-
-static inline
-void API_CALL(wait_contrib_comp_2d)(Sopalin_Data_t *sopalin_data,
-                                    pastix_int_t me, pastix_int_t i){
-
-  SolverMatrix  *datacode    = sopalin_data->datacode;
-#ifdef TRACE_SOPALIN
-  Thread_Data_t *thread_data = sopalin_data->thread_data[me];
-#endif
-#ifdef SMP_SOPALIN
-  pastix_int_t            firsttask   = TASK_MASTER(i);
-#endif
-
-  /* Attente contribution locale et MPI */
-#if (defined FORCE_CONSO)
-  if (THREAD_FUNNELED_OFF)
-    {
-      while ((!(TASK_BTAGPTR(i)))
-             || (!(RTASK_COEFTAB(i))))
-        {
-          API_CALL(rcsd_testall_fab)(sopalin_data, me);
-        }
-    }
-  else
-#endif
-    {
-      if (THREAD_COMM_OFF)
-        {
-          while ((TASK_BTAGPTR(i) == NULL) && (sopalin_data->taskmark[i] > 0))
-            {
-              ASSERTDBG(i == firsttask, MOD_SOPALIN);
-              RECV_ONE_BLOCK;
-            }
-        }
-    }
-
-  trace_begin_task(thread_data->tracefile,
-                   SOPALIN_CLOCK_TRACE, SOLV_PROCNUM, me, 1,
-                   STATE_WAITLOC, i);
-
-#if (DBG_PASTIX_DYNSCHED > 0)
-  MUTEX_LOCK(&(sopalin_data->mutex_task[i]));
-  ASSERTDBG(((TASK_TASKID(i) == E2) && (sopalin_data->taskmark[i] == 0))
-            || ((TASK_TASKID(i) == E1) &&
-                (sopalin_data->taskmark[i] == 1)), MOD_SOPALIN);
-  ASSERTDBG(TASK_BTAGPTR(i)  != NULL, MOD_SOPALIN);
-  ASSERTDBG(RTASK_COEFTAB(i) != NULL, MOD_SOPALIN);
-  sopalin_data->taskmark[i]++;
-  MUTEX_UNLOCK(&(sopalin_data->mutex_task[i]));
-#endif
-
-  MUTEX_LOCK(&(sopalin_data->mutex_task[firsttask]));
-  while ((!(TASK_BTAGPTR(i)))
-         || (!(RTASK_COEFTAB(i))))
-  COND_WAIT(&(sopalin_data->cond_task[firsttask]),
-            &(sopalin_data->mutex_task[firsttask]));
-  MUTEX_UNLOCK(&(sopalin_data->mutex_task[firsttask]));
 }
