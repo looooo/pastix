da61facd20f695ea422ad1e439b15190eed694b2
Xavier Lacoste <xl64100@gmail.com>
2014-07-23 13:19:07 +0200

    revert CHECK_MPI/CHECK_THREAD_LEVEL from old PaStiX version

--- pastix/include/pastix/retro.h
+++ pastix/include/pastix/retro.h
@@ -13,11 +13,98 @@
 /* #define PASTIX_PREFIX(x)     _PASTIX_ ## x */
 /* #define PASTIX_EXTERN(x)     x */
 
-#define CHECK_MPI(call) call
+#ifdef PASTIX_HAVE_MPI
+#  define CHECK_MPI(call) do {                                          \
+        int error_code;                                                 \
+        error_code = call;                                              \
+        if (error_code != MPI_SUCCESS) {                                \
+                                                                        \
+            char error_string[MPI_MAX_ERROR_STRING];                    \
+            int length_of_error_string, error_class;                    \
+            int my_rank = -1;                                           \
+                                                                        \
+            MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);                    \
+            MPI_Error_class(error_code, &error_class);                  \
+            MPI_Error_string(error_class, error_string,                 \
+                             &length_of_error_string);                  \
+            fprintf(stderr, "%3d: %s\n", my_rank, error_string);        \
+            MPI_Error_string(error_code, error_string,                  \
+                             &length_of_error_string);                  \
+            fprintf(stderr, "%3d: %s\n", my_rank, error_string);        \
+            MPI_Abort(MPI_COMM_WORLD, MPI_ERR);                         \
+        }                                                               \
+    } while(0)
+#else
+#  define CHECK_MPI(call) call
+#endif
+#if (defined PASTIX_HAVE_MPI && defined PASTIX_HAVE_PTHREAD)
+#    define CHECK_THREAD_LEVEL(THREAD_MODE)                             \
+  do {                                                                  \
+    int      provided;                                                  \
+                                                                        \
+    CHECK_MPI(MPI_Query_thread(&provided));                             \
+    if (THREAD_MODE == API_THREAD_FUNNELED)                             \
+      {                                                                 \
+        switch(provided) {                                              \
+        case MPI_THREAD_SINGLE:                                         \
+          errorPrint("This run only supports MPI_THREAD_SINGLE\n"       \
+                     "  either use -DFORCE_NOSMP,\n"                    \
+                     "  change your MPI Library\n"                      \
+                     "  or check that MPI_Init_thread"                  \
+                     " is correctly called\n");                         \
+          MPI_Abort(MPI_COMM_WORLD, MPI_ERR);                           \
+          break;                                                        \
+        case MPI_THREAD_FUNNELED:                                       \
+        case MPI_THREAD_SERIALIZED:                                     \
+        case MPI_THREAD_MULTIPLE:                                       \
+          break;                                                        \
+        default:                                                        \
+          errorPrint("provided thread level support is unknown");       \
+          MPI_Abort(MPI_COMM_WORLD, MPI_ERR);                           \
+          break;                                                        \
+        }                                                               \
+      }                                                                 \
+    else                                                                \
+      {                                                                 \
+        switch(provided) {                                              \
+        case MPI_THREAD_SINGLE:                                         \
+          errorPrint("This run only supports MPI_THREAD_SINGLE\n"       \
+                     "  either use -DFORCE_NOSMP,\n"                    \
+                     "  change your MPI Library\n"                      \
+                     "  or check that MPI_Init_thread"                  \
+                     " is correctly called\n");                         \
+          MPI_Abort(MPI_COMM_WORLD, MPI_ERR);                           \
+          break;                                                        \
+        case MPI_THREAD_FUNNELED:                                       \
+          errorPrint("This run only supports MPI_THREAD_FUNNELED\n"     \
+                     "  either use API_THREAD_FUNNELED,\n"              \
+                     "  change your MPI Library\n"                      \
+                     "  or check that MPI_Init_thread"                  \
+                     " is correctly called\n");                         \
+          MPI_Abort(MPI_COMM_WORLD, MPI_ERR);                           \
+          break;                                                        \
+        case MPI_THREAD_SERIALIZED:                                     \
+          errorPrint("This run only supports MPI_THREAD_SERIALIZED\n"   \
+                     "  either use API_THREAD_FUNNELED,\n"              \
+                     "  change your MPI Library\n"                      \
+                     "  or check that MPI_Init_thread"                  \
+                     " is correctly called\n");                         \
+          MPI_Abort(MPI_COMM_WORLD, MPI_ERR);                           \
+          break;                                                        \
+        case MPI_THREAD_MULTIPLE:                                       \
+          break;                                                        \
+        default:                                                        \
+          errorPrint("provided thread level support is unknown");       \
+          MPI_Abort(MPI_COMM_WORLD, MPI_ERR);                           \
+          break;                                                        \
+        }                                                               \
+      }                                                                 \
+  } while(0)
+#else
 #  define CHECK_THREAD_LEVEL(THREAD_MODE) do {  \
     THREAD_MODE = THREAD_MODE;                  \
   } while (0)
-
+#endif
 #define CONJ_FLOAT(val) (val)
 #define ABS_FLOAT(val)  fabs(val)
 
