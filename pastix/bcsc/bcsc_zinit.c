63543d65cfadf5d7babb68dfa084c6f556111e25
Mathieu Faverge <mathieu.faverge@inria.fr>
2014-08-28 20:14:56 +0200

    The centralized generation of blocked csc works again

--- pastix/bcsc/bcsc_zinit.c
+++ pastix/bcsc/bcsc_zinit.c
@@ -22,33 +22,37 @@
      * rows. The upper triangular part is done later if required through LU
      * factorization.
      */
-void
-bcsc_zInitLvalues( const pastix_csc_t       *csc,
-                   const Order              *ord,
-                   const SolverMatrix       *solvmtx,
-                   const pastix_int_t       *col2cblk,
-                         pastix_bcsc_t      *bcsc,
-                         pastix_complex64_t *avals,
-                         pastix_complex64_t *Lvalues )
+static inline void
+bcsc_zInitA( const pastix_csc_t  *csc,
+             const Order         *ord,
+             const SolverMatrix  *solvmtx,
+             const pastix_int_t  *col2cblk,
+                   pastix_bcsc_t *bcsc )
 {
+    pastix_complex64_t *avals   = (pastix_complex64_t*)(csc->avals);
+    pastix_complex64_t *Lvalues = (pastix_complex64_t*)(bcsc->Lvalues);
     pastix_int_t itercblk, itercol, baseval;
     pastix_int_t i, ival, idofcol, idofrow;
     int dof = csc->dof;
 
     baseval = csc->colptr[0];
 
+    /**
+     * Initialize the value of the matrix A in the blocked csc format. This
+     * applies the permutation to the avals array.
+     */
     for (itercol=0; itercol<csc->gN; itercol++)
     {
-        bcsc_format_t *bcblk;
-        pastix_int_t fcolnum, frow, lrow;
-        pastix_int_t itercol2 = ord->permtab[itercol] * dof;
+        pastix_int_t *coltab;
+        pastix_int_t  fcolnum, frow, lrow;
+        pastix_int_t  itercol2 = ord->permtab[itercol] * dof;
         itercblk = col2cblk[ itercol2 ];
 
         /* The block column is not stored locally, we skip it */
         if (itercblk == -1)
             continue;
 
-        bcblk   = &(bcsc->cscftab[itercblk]);
+        coltab  = bcsc->cscftab[itercblk].coltab;
         fcolnum = solvmtx->cblktab[itercblk].fcolnum;
 
         frow = csc->colptr[itercol]   - baseval;
@@ -64,7 +68,7 @@ bcsc_zInitLvalues( const pastix_csc_t       *csc,
             {
                 pastix_int_t colidx = itercol2 + idofcol - fcolnum;
                 pastix_int_t rowidx = iterrow2;
-                pastix_int_t pos = bcblk->coltab[ colidx ];
+                pastix_int_t pos = coltab[ colidx ];
 
                 for (idofrow = 0; idofrow < dof;
                      idofrow++, ival++, rowidx++, pos++)
@@ -73,9 +77,288 @@ bcsc_zInitLvalues( const pastix_csc_t       *csc,
                     Lvalues[ pos ] = avals[ ival ];
                 }
 
-                bcblk->coltab[ colidx ] += dof;
-                assert( bcblk->coltab[ colidx ] <= bcblk->coltab[ colidx+1 ] );
+                coltab[ colidx ] += dof;
+                assert( coltab[ colidx ] <= coltab[ colidx+1 ] );
             }
         }
+    }
+}
+
+static inline void
+bcsc_zInitLt( const pastix_csc_t  *csc,
+              const Order         *ord,
+              const SolverMatrix  *solvmtx,
+              const pastix_int_t  *col2cblk,
+                    pastix_bcsc_t *bcsc )
+{
+    pastix_complex64_t *avals   = (pastix_complex64_t*)(csc->avals);
+    pastix_complex64_t *Lvalues = (pastix_complex64_t*)(bcsc->Lvalues);
+    pastix_int_t itercblk, itercol, baseval;
+    pastix_int_t i, ival, idofcol, idofrow;
+    int dof = csc->dof;
+
+    baseval = csc->colptr[0];
+
+    /**
+     * Initialize the value of the matrix A^t in the blocked csc format. This
+     * applies the permutation to the avals array.
+     */
+    for (itercol=0; itercol<csc->gN; itercol++)
+    {
+        pastix_int_t frow, lrow;
+        pastix_int_t itercol2 = ord->permtab[itercol] * dof;
+
+        frow = csc->colptr[itercol]   - baseval;
+        lrow = csc->colptr[itercol+1] - baseval;
+
+        for (i=frow; i<lrow; i++)
+        {
+            pastix_int_t *coltab;
+            pastix_int_t fcolnum;
+            pastix_int_t iterrow  = csc->rows[i]-baseval;
+            pastix_int_t iterrow2 = ord->permtab[iterrow] * dof;
+
+            itercblk = col2cblk[ iterrow2 ];
+
+            /* The block column is not stored locally, we skip it */
+            if ((itercblk == -1) || (iterrow == itercol))
+                continue;
+
+            coltab  = bcsc->cscftab[itercblk].coltab;
+            fcolnum = solvmtx->cblktab[itercblk].fcolnum;
+
+            ival = i * dof * dof;
+
+            for (idofcol = 0; idofcol < dof; idofcol++)
+            {
+                pastix_int_t colidx = itercol2 + idofcol;
+                pastix_int_t rowidx = iterrow2 - fcolnum;
+                pastix_int_t pos;
+
+                for (idofrow = 0; idofrow < dof;
+                     idofrow++, ival++, rowidx++, pos++)
+                {
+                    pos = coltab[ rowidx ];
+
+                    bcsc->rowtab[ pos ] = colidx;
+                    Lvalues[ pos ] = avals[ ival ];
+
+                    coltab[ rowidx ]++;
+                }
+            }
+        }
+    }
+}
+
+#if defined(PRECISION_z) || defined(PRECISION_c)
+static inline void
+bcsc_zInitLh( const pastix_csc_t  *csc,
+              const Order         *ord,
+              const SolverMatrix  *solvmtx,
+              const pastix_int_t  *col2cblk,
+                    pastix_bcsc_t *bcsc )
+{
+    pastix_complex64_t *avals   = (pastix_complex64_t*)(csc->avals);
+    pastix_complex64_t *Lvalues = (pastix_complex64_t*)(bcsc->Lvalues);
+    pastix_int_t itercblk, itercol, baseval;
+    pastix_int_t i, ival, idofcol, idofrow;
+    int dof = csc->dof;
+
+    baseval = csc->colptr[0];
+
+    /**
+     * Initialize the value of the matrix A^t in the blocked csc format. This
+     * applies the permutation to the avals array.
+     */
+    for (itercol=0; itercol<csc->gN; itercol++)
+    {
+        pastix_int_t frow, lrow;
+        pastix_int_t itercol2 = ord->permtab[itercol] * dof;
+
+        frow = csc->colptr[itercol]   - baseval;
+        lrow = csc->colptr[itercol+1] - baseval;
+
+        for (i=frow; i<lrow; i++)
+        {
+            pastix_int_t *coltab;
+            pastix_int_t fcolnum;
+            pastix_int_t iterrow  = csc->rows[i]-baseval;
+            pastix_int_t iterrow2 = ord->permtab[iterrow] * dof;
+
+            itercblk = col2cblk[ iterrow2 ];
+
+            /* The block column is not stored locally, we skip it */
+            if ((itercblk == -1) || (iterrow == itercol))
+                continue;
+
+            coltab  = bcsc->cscftab[itercblk].coltab;
+            fcolnum = solvmtx->cblktab[itercblk].fcolnum;
+
+            ival = i * dof * dof;
+
+            for (idofcol = 0; idofcol < dof; idofcol++)
+            {
+                pastix_int_t colidx = itercol2 + idofcol;
+                pastix_int_t rowidx = iterrow2 - fcolnum;
+                pastix_int_t pos;
+
+                for (idofrow = 0; idofrow < dof;
+                     idofrow++, ival++, rowidx++, pos++)
+                {
+                    pos = coltab[ rowidx ];
+
+                    bcsc->rowtab[ pos ] = colidx;
+                    Lvalues[ pos ] = conj( avals[ ival ] );
+
+                    coltab[ rowidx ]++;
+                }
+            }
+        }
+    }
+}
+#endif /* defined(PRECISION_z) || defined(PRECISION_c) */
+
+void
+bcsc_zInitAt( const pastix_csc_t  *csc,
+              const Order         *ord,
+              const SolverMatrix  *solvmtx,
+              const pastix_int_t  *col2cblk,
+                    pastix_int_t  *trowtab,
+                    pastix_bcsc_t *bcsc )
+{
+    pastix_complex64_t *avals   = (pastix_complex64_t*)(csc->avals);
+    pastix_complex64_t *Uvalues = (pastix_complex64_t*)(bcsc->Uvalues);
+    pastix_int_t itercblk, itercol, baseval;
+    pastix_int_t i, ival, idofcol, idofrow;
+    int dof = csc->dof;
+
+    baseval = csc->colptr[0];
+
+    /**
+     * Initialize the value of the matrix A^t in the blocked csc format. This
+     * applies the permutation to the avals array.
+     */
+    for (itercol=0; itercol<csc->gN; itercol++)
+    {
+        pastix_int_t frow, lrow;
+        pastix_int_t itercol2 = ord->permtab[itercol] * dof;
+
+        frow = csc->colptr[itercol]   - baseval;
+        lrow = csc->colptr[itercol+1] - baseval;
+
+        for (i=frow; i<lrow; i++)
+        {
+            pastix_int_t *coltab;
+            pastix_int_t fcolnum;
+            pastix_int_t iterrow  = csc->rows[i]-baseval;
+            pastix_int_t iterrow2 = ord->permtab[iterrow] * dof;
+
+            itercblk = col2cblk[ iterrow2 ];
+
+            /* The block column is not stored locally, we skip it */
+            if ((itercblk == -1) || (iterrow == itercol))
+                continue;
+
+            coltab  = bcsc->cscftab[itercblk].coltab;
+            fcolnum = solvmtx->cblktab[itercblk].fcolnum;
+
+            ival = i * dof * dof;
+
+            for (idofcol = 0; idofcol < dof; idofcol++)
+            {
+                pastix_int_t colidx = itercol2 + idofcol;
+                pastix_int_t rowidx = iterrow2 - fcolnum;
+                pastix_int_t pos;
+
+                for (idofrow = 0; idofrow < dof;
+                     idofrow++, ival++, rowidx++, pos++)
+                {
+                    pos = coltab[ rowidx ];
+
+                    trowtab[ pos ] = colidx;
+                    Uvalues[ pos ] = avals[ ival ];
+
+                    coltab[ rowidx ]++;
+                }
+            }
+        }
+    }
+}
+
+static inline void
+bcsc_zSort( const pastix_bcsc_t *bcsc,
+            pastix_int_t        *rowtab,
+            pastix_complex64_t  *valtab )
+{
+    bcsc_format_t *blockcol;
+    pastix_int_t itercblk, itercol, size;
+    void *sortptr[2];
+
+    blockcol = bcsc->cscftab;
+    for (itercblk=0; itercblk<bcsc->cscfnbr; itercblk++, blockcol++)
+    {
+        for (itercol=0; itercol<blockcol->colnbr; itercol++)
+        {
+            sortptr[0] = (void*)(rowtab + blockcol->coltab[itercol]);
+            sortptr[1] = (void*)(valtab + blockcol->coltab[itercol]);
+
+            size = blockcol->coltab[itercol+1] - blockcol->coltab[itercol];
+
+            z_qsortIntFloatAsc( sortptr, size );
+        }
+    }
+}
+
+void
+bcsc_zInitCentralized( const pastix_csc_t  *csc,
+                       const Order         *ord,
+                       const SolverMatrix  *solvmtx,
+                       const pastix_int_t  *col2cblk,
+                             int            initAt,
+                             pastix_bcsc_t *bcsc )
+{
+    pastix_int_t valuesize;
+
+    valuesize = bcsc_init_centralized_coltab( csc, ord, solvmtx, bcsc );
+
+    /**
+     * Initialize the blocked structure of the matrix A
+     */
+    bcsc_zInitA( csc, ord, solvmtx, col2cblk, bcsc );
+    if ( csc->mtxtype == PastixSymmetric ) {
+        bcsc_zInitLt( csc, ord, solvmtx, col2cblk, bcsc );
+    }
+#if defined(PRECISION_z) || defined(PRECISION_c)
+    else if ( csc->mtxtype == PastixHermitian ) {
+        bcsc_zInitLh( csc, ord, solvmtx, col2cblk, bcsc );
+    }
+#endif /* defined(PRECISION_z) || defined(PRECISION_c) */
+
+    /* Restore the correct coltab arrays */
+    bcsc_restore_coltab( bcsc );
+
+    /* Sort the csc */
+    bcsc_zSort( bcsc, bcsc->rowtab, bcsc->Lvalues );
+
+    if ( csc->mtxtype == PastixGeneral ) {
+	/* A^t is not required if only refinment is performed */
+        if (initAt) {
+            pastix_int_t *trowtab;
+            MALLOC_INTERN( bcsc->Uvalues, valuesize * pastix_size_of( bcsc->flttype ), char );
+            MALLOC_INTERN( trowtab, valuesize, pastix_int_t);
+
+            bcsc_zInitAt( csc, ord, solvmtx, col2cblk, trowtab, bcsc );
+
+            /* Restore the correct coltab arrays */
+            bcsc_restore_coltab( bcsc );
+
+	    /* Sort the transposed csc */
+	    bcsc_zSort( bcsc, trowtab, bcsc->Uvalues );
+	    memFree( trowtab );
+        }
+    }
+    else {
+        /* In case of PastixHermitian, conj is applied when used to save memory space */
+        bcsc->Uvalues = bcsc->Lvalues;
     }
 }
