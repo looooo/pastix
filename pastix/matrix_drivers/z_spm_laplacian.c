52ba646500e39c9c1acac9dba50c460a649481f7
Theophile Terraz <theophile.terraz@inria.fr>
2015-03-09 14:31:35 +0100

    add extended 3D laplacian

--- pastix/matrix_drivers/z_spm_laplacian.c
+++ pastix/matrix_drivers/z_spm_laplacian.c
@@ -32,7 +32,8 @@
  *******************************************************************************
  *
  * @param[in,out] csc
- *          At start, contains the size of the laplacian in csc->n.
+ *          At start, an allocated csc structure.
+ *          Contains the size of the laplacian in csc->n.
  *          At exit, contains the matrix in csc format.
  *
  * @param[in] dim1
@@ -124,7 +125,8 @@ z_spmLaplacian1D( pastix_csc_t  *csc,
  *******************************************************************************
  *
  * @param[in,out] csc
- *          At start, contains the size of the laplacian in csc->n.
+ *          At start, an allocated csc structure.
+ *          Contains the size of the laplacian in csc->n.
  *          At exit, contains the matrix in csc format.
  *
  * @param[in] dim1
@@ -234,7 +236,8 @@ z_spmLaplacian2D( pastix_csc_t  *csc,
  *******************************************************************************
  *
  * @param[in,out] csc
- *          At start, contains the size of the laplacian in csc->n.
+ *          At start, an allocated csc structure.
+ *          Contains the size of the laplacian in csc->n.
  *          At exit, contains the matrix in csc format.
  *
  * @param[in] dim1
@@ -354,7 +357,8 @@ z_spmLaplacian3D( pastix_csc_t  *csc,
  *******************************************************************************
  *
  * @param[in,out] csc
- *          At start, contains the size of the laplacian in csc->n.
+ *          At start, an allocated csc structure.
+ *          Contains the size of the laplacian in csc->n.
  *          At exit, contains the matrix in csc format.
  *
  * @param[in] dim1
@@ -366,13 +370,13 @@ z_spmLaplacian3D( pastix_csc_t  *csc,
  *******************************************************************************/
 void
 z_spmExtendedLaplacian2D( pastix_csc_t  *csc,
-                  pastix_int_t   dim1,
-                  pastix_int_t   dim2 )
+                          pastix_int_t   dim1,
+                          pastix_int_t   dim2 )
 {
     pastix_complex64_t *valptr;
     pastix_int_t *colptr, *rowptr;
     pastix_int_t i, j, k;
-    pastix_int_t nnz = (2*(dim1)-1)*dim2 + (dim2-1)*dim1;
+    pastix_int_t nnz = (2*(dim1)-1)*dim2 + (dim2-1)*(3*dim1-2);
 
     csc->mtxtype  = PastixSymmetric;
     csc->flttype  = PastixComplex64;
@@ -430,11 +434,11 @@ z_spmExtendedLaplacian2D( pastix_csc_t  *csc,
             }
 
             /* Connexion along dimension 2 */
-            if (i < dim2) {
-                *rowptr = k+dim1;
-                
+            if (i < dim2)
+            {
                 if (j > 1)
                 {
+                    *rowptr = k+dim1-1;
 #if !defined(PRECISION_p)
                     *valptr = (pastix_complex64_t)-0.5;
 #endif
@@ -442,6 +446,7 @@ z_spmExtendedLaplacian2D( pastix_csc_t  *csc,
                     
                 }
                 
+                *rowptr = k+dim1;
 #if !defined(PRECISION_p)
                 *valptr = (pastix_complex64_t)-1.;
 #endif
@@ -449,6 +454,7 @@ z_spmExtendedLaplacian2D( pastix_csc_t  *csc,
                 
                 if (j < dim1)
                 {
+                    *rowptr = k+dim1+1;
 #if !defined(PRECISION_p)
                     *valptr = (pastix_complex64_t)-0.5;
 #endif
@@ -458,6 +464,234 @@ z_spmExtendedLaplacian2D( pastix_csc_t  *csc,
             }
 
             colptr++; k++;
+        }
+    }
+
+    assert( (csc->colptr[ csc->gN ] - csc->colptr[0]) == nnz );
+}
+
+/**
+ *******************************************************************************
+ *
+ * @ingroup pastix_csc_driver
+ *
+ * z_spmExtendedLaplacian3D - Generate a 3D extended laplacian matrix.
+ *
+ *******************************************************************************
+ *
+ * @param[in,out] csc
+ *          At start, an allocated csc structure.
+ *          Contains the size of the laplacian in csc->n.
+ *          At exit, contains the matrix in csc format.
+ *
+ * @param[in] dim1
+ *          contains the first dimension of the 3D grid of the laplacian.
+ *
+ * @param[in] dim2
+ *          contains the second dimension of the 3D grid of the laplacian.
+ *
+ * @param[in] dim3
+ *          contains the second dimension of the 3D grid of the laplacian.
+ *
+ *******************************************************************************/
+void
+z_spmExtendedLaplacian3D( pastix_csc_t  *csc,
+                          pastix_int_t   dim1,
+                          pastix_int_t   dim2,
+                          pastix_int_t   dim3 )
+{
+
+    pastix_complex64_t *valptr;
+    pastix_int_t *colptr, *rowptr;
+    pastix_int_t i, j, k, l;
+    pastix_int_t nnz = (2*dim1-1)*dim2*dim3 + (3*dim1-2)*(dim2-1)*dim3 + (3*dim1-2)*(dim2-1)*(dim3-1);
+
+    csc->mtxtype  = PastixSymmetric;
+    csc->flttype  = PastixComplex64;
+    csc->fmttype  = PastixCSC;
+    csc->gnnz     = nnz;
+    csc->nnz      = nnz;
+    csc->dof      = 1;
+
+    assert( csc->gN == dim1*dim2*dim3 );
+
+    /* Allocating */
+    csc->colptr = malloc((csc->n+1)*sizeof(pastix_int_t));
+    csc->rows   = malloc(nnz       *sizeof(pastix_int_t));
+    assert( csc->colptr );
+    assert( csc->rows   );
+
+#if !defined(PRECISION_p)
+    csc->avals  = malloc(nnz       *sizeof(pastix_complex64_t));
+    assert( csc->avals  );
+#endif
+
+    /* Building ia, ja and avals*/
+    colptr = csc->colptr;
+    rowptr = csc->rows;
+    valptr = (pastix_complex64_t*)(csc->avals);
+
+    /* Building ia, ja and avals */
+    *colptr = 1;
+    l = 1; /* Column index in the matrix ((i-1) * dim1 * dim2 + (j-1) * dim1 + k-1) */
+    for(i=1; i<=dim3; i++)
+    {
+        for(j=1; j<=dim2; j++)
+        {
+            for(k=1; k<=dim1; k++)
+            {
+
+                colptr[1] = colptr[0];
+
+                /* Diagonal value */
+                *rowptr = l;
+#if !defined(PRECISION_p)
+            if ( (j == dim2 || j == 1) && (i == dim3 || i == 1) && (k == dim1 || i == 1) )
+                *valptr = (pastix_complex64_t) 4.75;
+            else if ( (j != dim2 || j != 1) && (i == dim3 || i == 1) && (k == dim1 || i == 1) )
+                *valptr = (pastix_complex64_t) 10.;
+            else if ( (j == dim2 || j == 1) && (i != dim3 || i != 1) && (k == dim1 || i == 1) )
+                *valptr = (pastix_complex64_t) 10.;
+            else if ( (j == dim2 || j == 1) && (i == dim3 || i == 1) && (k != dim1 || i != 1) )
+                *valptr = (pastix_complex64_t) 10.;
+            else if ( (j != dim2 || j != 1) && (i != dim3 || i != 1) && (k == dim1 || i == 1) )
+                *valptr = (pastix_complex64_t) 7.;
+            else if ( (j == dim2 || j == 1) && (i != dim3 || i != 1) && (k != dim1 || i != 1) )
+                *valptr = (pastix_complex64_t) 7.;
+            else if ( (j != dim2 || j != 1) && (i == dim3 || i == 1) && (k != dim1 || i != 1) )
+                *valptr = (pastix_complex64_t) 7.;
+            else
+                *valptr = (pastix_complex64_t) 14.;
+#endif
+                valptr++; rowptr++; colptr[1]++;
+
+                /* Connexion along dimension 1 */
+                if (k < dim1) {
+                    *rowptr = l+1;
+#if !defined(PRECISION_p)
+                    *valptr = (pastix_complex64_t)-1.;
+#endif
+                    valptr++; rowptr++; colptr[1]++;
+                }
+
+                /* Connexion along dimension 2 */
+                if (j < dim2)
+                {
+                    if (k > 1)
+                    {
+                        *rowptr = l+dim1-1;
+#if !defined(PRECISION_p)
+                        *valptr = (pastix_complex64_t)-0.5;
+#endif
+                        valptr++; rowptr++; colptr[1]++;
+                        
+                    }
+                    
+                    *rowptr = l+dim1;
+#if !defined(PRECISION_p)
+                    *valptr = (pastix_complex64_t)-1.;
+#endif
+                    valptr++; rowptr++; colptr[1]++;
+                
+                    if (k < dim1)
+                    {
+                        *rowptr = l+dim1+1;
+#if !defined(PRECISION_p)
+                        *valptr = (pastix_complex64_t)-0.5;
+#endif
+                        valptr++; rowptr++; colptr[1]++;
+                        
+                    }
+                }
+
+                /* Connexion along dimension 3 */
+                if (i < dim3) {
+                    if( j > 1 )
+                    {
+                        if (k > 1)
+                        {
+                            *rowptr = l+dim1*dim2-dim1-1;
+#if !defined(PRECISION_p)
+                            *valptr = (pastix_complex64_t)-0.25;
+#endif
+                            valptr++; rowptr++; colptr[1]++;
+                            
+                        }
+
+                        *rowptr = l+dim1*dim2-dim1;
+#if !defined(PRECISION_p)
+                        *valptr = (pastix_complex64_t)-0.5;
+#endif
+                        valptr++; rowptr++; colptr[1]++;
+
+                        if (k < dim1)
+                        {
+                            *rowptr = l+dim1*dim2-dim1+1;
+#if !defined(PRECISION_p)
+                            *valptr = (pastix_complex64_t)-0.25;
+#endif
+                            valptr++; rowptr++; colptr[1]++;
+
+                        }
+                    }
+                    if (k > 1)
+                    {
+                        *rowptr = l+dim1*dim2-1;
+#if !defined(PRECISION_p)
+                        *valptr = (pastix_complex64_t)-0.5;
+#endif
+                        valptr++; rowptr++; colptr[1]++;
+                        
+                    }
+
+                    *rowptr = l+dim1*dim2;
+#if !defined(PRECISION_p)
+                    *valptr = (pastix_complex64_t)-1.;
+#endif
+                    valptr++; rowptr++; colptr[1]++;
+
+                    if (k < dim1)
+                    {
+                        *rowptr = l+dim1*dim2+1;
+#if !defined(PRECISION_p)
+                        *valptr = (pastix_complex64_t)-0.5;
+#endif
+                        valptr++; rowptr++; colptr[1]++;
+                        
+                    }
+
+                    if( j < dim2 )
+                    {
+                        if (k > 1)
+                        {
+                            *rowptr = l+dim1*dim2+dim1-1;
+#if !defined(PRECISION_p)
+                            *valptr = (pastix_complex64_t)-0.25;
+#endif
+                            valptr++; rowptr++; colptr[1]++;
+
+                        }
+
+                        *rowptr = l+dim1*dim2+dim1;
+#if !defined(PRECISION_p)
+                        *valptr = (pastix_complex64_t)-0.5;
+#endif
+                        valptr++; rowptr++; colptr[1]++;
+
+                        if (k < dim1)
+                        {
+                            *rowptr = l+dim1*dim2+dim1+1;
+#if !defined(PRECISION_p)
+                            *valptr = (pastix_complex64_t)-0.25;
+#endif
+                            valptr++; rowptr++; colptr[1]++;
+
+                        }
+                    }
+                }
+
+                colptr++; l++;
+            }
         }
     }
 
