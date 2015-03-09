044c74497a4f5cfe02b16cf1fc1ade0fe1799240
Theophile Terraz <theophile.terraz@inria.fr>
2015-03-09 10:28:33 +0100

    add extended 2D laplacian

--- pastix/matrix_drivers/z_spm_laplacian.c
+++ pastix/matrix_drivers/z_spm_laplacian.c
@@ -343,3 +343,123 @@ z_spmLaplacian3D( pastix_csc_t  *csc,
 
     assert( (csc->colptr[ csc->gN ] - csc->colptr[0]) == nnz );
 }
+
+/**
+ *******************************************************************************
+ *
+ * @ingroup pastix_csc_driver
+ *
+ * z_spmExtendedLaplacian2D - Generate a 2D extended laplacian matrix.
+ *
+ *******************************************************************************
+ *
+ * @param[in,out] csc
+ *          At start, contains the size of the laplacian in csc->n.
+ *          At exit, contains the matrix in csc format.
+ *
+ * @param[in] dim1
+ *          contains the first dimension of the 2D grid of the laplacian.
+ *
+ * @param[in] dim2
+ *          contains the second dimension of the 2D grid of the laplacian.
+ *
+ *******************************************************************************/
+void
+z_spmExtendedLaplacian2D( pastix_csc_t  *csc,
+                  pastix_int_t   dim1,
+                  pastix_int_t   dim2 )
+{
+    pastix_complex64_t *valptr;
+    pastix_int_t *colptr, *rowptr;
+    pastix_int_t i, j, k;
+    pastix_int_t nnz = (2*(dim1)-1)*dim2 + (dim2-1)*dim1;
+
+    csc->mtxtype  = PastixSymmetric;
+    csc->flttype  = PastixComplex64;
+    csc->fmttype  = PastixCSC;
+    csc->gnnz     = nnz;
+    csc->nnz      = nnz;
+    csc->dof      = 1;
+
+    assert( csc->gN == dim1*dim2 );
+
+    /* Allocating */
+    csc->colptr = malloc((csc->n+1)*sizeof(pastix_int_t));
+    csc->rows   = malloc(nnz       *sizeof(pastix_int_t));
+    assert( csc->colptr );
+    assert( csc->rows   );
+
+#if !defined(PRECISION_p)
+    csc->avals  = malloc(nnz       *sizeof(pastix_complex64_t));
+    assert( csc->avals  );
+#endif
+
+    /* Building ia, ja and avals*/
+    colptr = csc->colptr;
+    rowptr = csc->rows;
+    valptr = (pastix_complex64_t*)(csc->avals);
+
+    /* Building ia, ja and avals */
+    *colptr = 1;
+    k = 1; /* Column index in the matrix ((i-1) * dim1 + j-1) */
+    for(i=1; i<=dim2; i++)
+    {
+        for(j=1; j<=dim1; j++)
+        {
+            colptr[1] = colptr[0];
+
+            /* Diagonal value */
+            *rowptr = k;
+#if !defined(PRECISION_p)
+            if ( (j == dim1 || j == 1) && (i == dim2 || i == 1) )
+                *valptr = (pastix_complex64_t) 2.5;
+            else if (j == dim1 || j == 1 || i == dim2 || i == 1)
+                *valptr = (pastix_complex64_t) 4.;
+            else
+                *valptr = (pastix_complex64_t) 6.;
+#endif
+            valptr++; rowptr++; colptr[1]++;
+
+            /* Connexion along dimension 1 */
+            if (j < dim1) {
+                *rowptr = k+1;
+#if !defined(PRECISION_p)
+                *valptr = (pastix_complex64_t)-1.;
+#endif
+                valptr++; rowptr++; colptr[1]++;
+            }
+
+            /* Connexion along dimension 2 */
+            if (i < dim2) {
+                *rowptr = k+dim1;
+                
+                if (j > 1)
+                {
+#if !defined(PRECISION_p)
+                    *valptr = (pastix_complex64_t)-0.5;
+#endif
+                    valptr++; rowptr++; colptr[1]++;
+                    
+                }
+                
+#if !defined(PRECISION_p)
+                *valptr = (pastix_complex64_t)-1.;
+#endif
+                valptr++; rowptr++; colptr[1]++;
+                
+                if (j < dim1)
+                {
+#if !defined(PRECISION_p)
+                    *valptr = (pastix_complex64_t)-0.5;
+#endif
+                    valptr++; rowptr++; colptr[1]++;
+                    
+                }
+            }
+
+            colptr++; k++;
+        }
+    }
+
+    assert( (csc->colptr[ csc->gN ] - csc->colptr[0]) == nnz );
+}
