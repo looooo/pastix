45eaf8c64c03a4310d7e4919d3afc040c9d06afb
Theophile Terraz <theophile.terraz@inria.fr>
2015-03-27 09:16:24 +0100

    add table sopalinFacto in pastix_task_sopalin

--- pastix/sopalin/pastix_task_sopalin.c
+++ pastix/sopalin/pastix_task_sopalin.c
@@ -19,13 +19,34 @@
 #include "bcsc.h"
 #include "sopalin_data.h"
 
-static void (*potrfTable[6])(sopalin_data_t*) = {
-    NULL,
-    NULL,
-    pastix_static_spotrf,
-    pastix_static_dpotrf,
-    pastix_static_cpotrf,
-    pastix_static_zpotrf };
+// static void (*potrfTable[6])(sopalin_data_t*) = {
+static void (*sopalinFacto[4][6])(sopalin_data_t*) = 
+{
+    {
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL 
+    },
+    {
+        NULL,
+        NULL,
+        pastix_static_spotrf,
+        pastix_static_dpotrf,
+        pastix_static_cpotrf,
+        pastix_static_zpotrf 
+    },
+    {
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL 
+    }
+};
 
 void
 coeftabInit( const SolverMatrix  *datacode,
@@ -157,7 +178,8 @@ pastix_task_sopalin( pastix_data_t *pastix_data,
         clockStart(timer);
         sopalin_data.solvmtx = pastix_data->solvmatr;
 
-        potrfTable[csc->flttype]( &sopalin_data );
+        sopalinFacto[csc->mtxtype][csc->flttype]( &sopalin_data );
+
         clockStop(timer);
         pastix_print( 0, 0, "--Factorization time: %g --\n", clockVal(timer));
 
