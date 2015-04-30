7d7958375014529459eda1148004c21a4847bd82
Mathieu Faverge <mathieu.faverge@inria.fr>
2015-04-30 10:55:26 +0200

    Small changes in pastix_task_reordering

--- pastix/fax/pastix_task_reordering.c
+++ pastix/fax/pastix_task_reordering.c
@@ -13,8 +13,9 @@ pastix_task_reordering(pastix_data_t *pastix_data, pastix_int_t split_level,
                        int stop_criteria, int stop_when_fitting)
 {
     Clock timer;
+
+    /* Compute the Reordering complexity */
     symbolPrintComplexityReordering( pastix_data->symbmtx );
-    //symbolPrintComplexityReordering2( pastix_data->symbmtx );
 
     clockStart(timer);
 
@@ -25,19 +26,19 @@ pastix_task_reordering(pastix_data_t *pastix_data, pastix_int_t split_level,
     /* int retval = orderSave( pastix_data->ordemesh, NULL ); */
 
     symbolExit(pastix_data->symbmtx);
-    free(pastix_data->symbmtx);
+    memFree_null(pastix_data->symbmtx);
     pastix_data->symbmtx = NULL;
 
     pastix_task_symbfact( pastix_data, NULL, NULL );
+
+    clockStop(timer);
+    printf("Total time for reordering (with extra symbolic factorization) %.3g s\n", (double)clockVal(timer));
 
     if (pastix_data->graph != NULL)
     {
         graphExit( pastix_data->graph );
         memFree_null( pastix_data->graph );
     }
-
-    clockStop(timer);
-    printf("Total time for reordering (with extra symbolic factorization) %.3g s\n", (double)clockVal(timer));
 
     return PASTIX_SUCCESS;
 }
