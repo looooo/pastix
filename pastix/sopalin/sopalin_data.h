f3f982caef743c09f33efea718838784edca4b53
Mathieu Faverge <mathieu.faverge@inria.fr>
2015-05-25 11:01:48 +0530

    Add threshold field to the sopalin_data structure and the declaration of the others algorithms

--- pastix/sopalin/sopalin_data.h
+++ pastix/sopalin/sopalin_data.h
@@ -19,14 +19,28 @@
 
 struct sopalin_data_s {
     SolverMatrix *solvmtx;
+    double        diagthreshold; /* Threshold for static pivoting on diagonal value */
 };
 typedef struct sopalin_data_s sopalin_data_t;
 
+
+void pastix_static_zgetrf( sopalin_data_t *sopalin_data );
+void pastix_static_cgetrf( sopalin_data_t *sopalin_data );
+void pastix_static_dgetrf( sopalin_data_t *sopalin_data );
+void pastix_static_sgetrf( sopalin_data_t *sopalin_data );
+
+void pastix_static_zhetrf( sopalin_data_t *sopalin_data );
+void pastix_static_chetrf( sopalin_data_t *sopalin_data );
 
 void pastix_static_zpotrf( sopalin_data_t *sopalin_data );
 void pastix_static_cpotrf( sopalin_data_t *sopalin_data );
 void pastix_static_dpotrf( sopalin_data_t *sopalin_data );
 void pastix_static_spotrf( sopalin_data_t *sopalin_data );
+
+void pastix_static_zsytrf( sopalin_data_t *sopalin_data );
+void pastix_static_csytrf( sopalin_data_t *sopalin_data );
+void pastix_static_dsytrf( sopalin_data_t *sopalin_data );
+void pastix_static_ssytrf( sopalin_data_t *sopalin_data );
 
 #endif /* _SOPALIN_DATA_H_ */
 
