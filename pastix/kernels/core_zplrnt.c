bbfecc0f4594e6801bc84def694e54bf06c3c8db
Mathieu Faverge <mathieu.faverge@inria.fr>
2015-06-19 09:53:57 +0200

    Add the plrnt kernel to generate random vectors. Will probably get modified in the future

---
+++ pastix/kernels/core_zplrnt.c
@@ -0,0 +1,107 @@
+/**
+ *
+ * @file core_zplrnt.c
+ *
+ *  PLASMA core_blas kernel
+ *  PLASMA is a software package provided by Univ. of Tennessee,
+ *  Univ. of California Berkeley and Univ. of Colorado Denver
+ *
+ * @version 2.7.1
+ * @author Piotr Luszczek
+ * @author Pierre Lemarinier
+ * @author Mathieu Faverge
+ * @date 2010-11-15
+ * @precisions normal z -> c d s
+ *
+ **/
+#include "common.h"
+
+#define Rnd64_A  6364136223846793005ULL
+#define Rnd64_C  1ULL
+#define RndF_Mul 5.4210108624275222e-20f
+#define RndD_Mul 5.4210108624275222e-20
+
+static inline unsigned long long int
+Rnd64_jump(unsigned long long int n, unsigned long long int seed ) {
+  unsigned long long int a_k, c_k, ran;
+  int i;
+
+  a_k = Rnd64_A;
+  c_k = Rnd64_C;
+
+  ran = seed;
+  for (i = 0; n; n >>= 1, ++i) {
+    if (n & 1)
+      ran = a_k * ran + c_k;
+    c_k *= (a_k + 1);
+    a_k *= a_k;
+  }
+
+  return ran;
+}
+
+#if defined(PRECISION_z) || defined(PRECISION_c)
+#define NBELEM   2
+#else
+#define NBELEM   1
+#endif
+
+/***************************************************************************//**
+ *
+ * @ingroup CORE_pastix_complex64_t
+ *
+ *  CORE_zplrnt generates a random tile.
+ *
+ *******************************************************************************
+ *
+ * @param[in] m
+ *         The number of rows of the tile A. m >= 0.
+ *
+ * @param[in] n
+ *         The number of columns of the tile A. n >= 0.
+ *
+ * @param[in,out] A
+ *         On entry, the m-by-n tile to be initialized.
+ *         On exit, the tile initialized in the mtxtype format.
+ *
+ * @param[in] lda
+ *         The leading dimension of the tile A. lda >= max(1,m).
+ *
+ * @param[in] gM
+ *         The global number of rows of the full matrix, A is belonging to. gM >= (m0+M).
+ *
+ * @param[in] m0
+ *         The index of the first row of tile A in the full matrix. m0 >= 0.
+ *
+ * @param[in] n0
+ *         The index of the first column of tile A in the full matrix. n0 >= 0.
+ *
+ * @param[in] seed
+ *         The seed used for random generation. Must be the same for
+ *         all tiles initialized with this routine.
+ *
+ ******************************************************************************/
+void CORE_zplrnt( int m, int n, pastix_complex64_t *A, int lda,
+                  int gM, int m0, int n0, unsigned long long int seed )
+{
+    pastix_complex64_t *tmp = A;
+    int64_t i, j;
+    unsigned long long int ran, jump;
+
+    jump = (unsigned long long int)m0 + (unsigned long long int)n0 * (unsigned long long int)gM;
+
+    for (j=0; j<n; ++j ) {
+        ran = Rnd64_jump( NBELEM*jump, seed );
+        for (i = 0; i < m; ++i) {
+            *tmp = 0.5f - ran * RndF_Mul;
+            ran  = Rnd64_A * ran + Rnd64_C;
+#ifdef COMPLEX
+            *tmp += I*(0.5f - ran * RndF_Mul);
+            ran   = Rnd64_A * ran + Rnd64_C;
+#endif
+            tmp++;
+        }
+        tmp  += lda-i;
+        jump += gM;
+    }
+}
