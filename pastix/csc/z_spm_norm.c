7e39047b241ce655b19dbefdc88c6f3fe964256c
Theophile Terraz <theophile.terraz@inria.fr>
2015-07-03 14:37:14 +0200

    restructurationreorganization

--- pastix/csc/z_spm_norm.c
+++ pastix/csc/z_spm_norm.c
@@ -18,48 +18,7 @@
 #include "common.h"
 #include "csc.h"
 #include "z_spm.h"
-
-/**
- *******************************************************************************
- *
- * @ingroup spm_internal
- *
- * frobenius_update - Update the couple (scale, sumsq) with one element when
- * computing the Froebnius norm.
- *
- * The frobenius norm is equal to scale * sqrt( sumsq ), this method allows to
- * avoid overflow in the sum square computation.
- *
- *******************************************************************************
- *
- * @param[in,out] scale
- *           On entry, the former scale
- *           On exit, the update scale to take into account the value
- *
- * @param[in,out] sumsq
- *           On entry, the former sumsq
- *           On exit, the update sumsq to take into account the value
- *
- * @param[in] value
- *          The value to integrate into the couple (scale, sumsq)
- *
- *******************************************************************************/
-static inline void
-frobenius_update( int nb, double *scale, double *sumsq, double *value )
-{
-    double absval = fabs(*value);
-    double ratio;
-    if ( absval != 0. ){
-        if ( (*scale) < absval ) {
-            ratio = (*scale) / absval;
-            *sumsq = (double)nb + (*sumsq) * ratio * ratio;
-            *scale = absval;
-        } else {
-            ratio = absval / (*scale);
-            *sumsq = (*sumsq) + (double)nb * ratio * ratio;
-        }
-    }
-}
+#include "frobeniusupdate.h"
 
 /**
  *******************************************************************************
