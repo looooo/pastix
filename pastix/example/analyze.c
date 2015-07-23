3a454ba079029027d8083a27e541c62eecabf8ef
Mathieu Faverge <mathieu.faverge@inria.fr>
2015-07-24 00:04:21 +0200

    Add comments to the analyze testing

--- pastix/example/analyze.c
+++ pastix/example/analyze.c
@@ -6,54 +6,17 @@
  *
  */
 #include <pastix.h>
-#include "../matrix_drivers/drivers.h"
 #include <csc.h>
+#include "../matrix_drivers/drivers.h"
 
 int main (int argc, char **argv)
 {
     pastix_data_t  *pastix_data = NULL; /* Pointer to a storage structure needed by pastix           */
     pastix_int_t    iparm[IPARM_SIZE];  /* integer parameters for pastix                             */
     double          dparm[DPARM_SIZE];  /* floating parameters for pastix                            */
-#if defined(PASTIX_WITH_MPI)
-    int             required;           /* MPI thread level required                                 */
-    int             provided;           /* MPI thread level provided                                 */
-#endif
-    int             mpid;
     pastix_driver_t driver;        /* Matrix driver(s) requested by user                        */
     char           *filename;           /* Filename(s) given by user                                 */
     pastix_csc_t    csc;
-
-    /*******************************************/
-    /*          MPI initialisation             */
-    /*******************************************/
-#if defined(PASTIX_WITH_MPI)
-    required = MPI_THREAD_MULTIPLE;
-    provided = -1;
-    MPI_Init_thread(&argc, &argv, required, &provided);
-    MPI_Comm_rank(MPI_COMM_WORLD, &mpid);
-    if (mpid == 0)
-    {
-        switch (provided)
-        {
-        case MPI_THREAD_SINGLE:
-            printf("MPI_Init_thread level = MPI_THREAD_SINGLE\n");
-            break;
-        case MPI_THREAD_FUNNELED:
-            printf("MPI_Init_thread level = MPI_THREAD_FUNNELED\n");
-            break;
-        case MPI_THREAD_SERIALIZED:
-            printf("MPI_Init_thread level = MPI_THREAD_SERIALIZED\n");
-            break;
-        case MPI_THREAD_MULTIPLE:
-            printf("MPI_Init_thread level = MPI_THREAD_MULTIPLE\n");
-            break;
-        default:
-            printf("MPI_Init_thread level = ???\n");
-        }
-    }
-#else
-    mpid = 0;
-#endif
 
     /**
      * Initialize parameters to default values
@@ -68,20 +31,21 @@ int main (int argc, char **argv)
                           iparm, dparm,
                           &driver, &filename );
 
+    /**
+     * Read the sparse matrix with the driver
+     */
     cscReadFromFile( driver, filename, &csc, MPI_COMM_WORLD );
     free(filename);
 
+    /**
+     * Perform ordering, symbolic factorization, and analyze steps
+     */
     pastix_task_order( pastix_data, &csc, NULL, NULL );
     pastix_task_symbfact( pastix_data, NULL, NULL );
     pastix_task_blend( pastix_data );
 
     spmExit( &csc );
-
     pastixFinalize( &pastix_data, MPI_COMM_WORLD, iparm, dparm );
-
-#if defined(PASTIX_WITH_MPI)
-    MPI_Finalize();
-#endif
 
     return EXIT_SUCCESS;
 }
