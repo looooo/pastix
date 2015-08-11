c869e0971cbf3a6695c2c9e15692ef8b85b554bb
Mathieu Faverge <mathieu.faverge@inria.fr>
2015-08-11 14:33:44 -0400

    Update the three other parsec wrappers

--- pastix/sopalin/parsec/zpotrf_sp1dplus_wrapper.c
+++ pastix/sopalin/parsec/zpotrf_sp1dplus_wrapper.c
@@ -6,57 +6,63 @@
  * @precisions normal z -> s d c
  *
  */
-#include "dague_internal.h"
-#include "dsparse.h"
-#include "data_dist/sparse-matrix/pastix_internal/pastix_internal.h"
+#include <dague.h>
+#include <dague/data_distribution.h>
+#include "common.h"
+#include "sopalin_data.h"
+#include "parsec/sparse-matrix.h"
 
-#include "memory_pool.h"
-#include "zpotrf_sp1dplus.h"
+typedef struct dague_zpotrf_sp1dplus_handle dague_zpotrf_sp1dplus_handle_t;
 
-dague_object_t*
-dsparse_zpotrf_sp_New(sparse_matrix_desc_t *A)
+extern dague_zpotrf_sp1dplus_handle_t *dague_zpotrf_sp1dplus_new(dague_ddesc_t * dataA /* data dataA */, sopalin_data_t * sopalin_data);
+
+dague_handle_t*
+dsparse_zpotrf_sp_New( sparse_matrix_desc_t *A,
+                       sopalin_data_t *sopalin_data )
 {
-    dague_zpotrf_sp1dplus_object_t *dague_zpotrf_sp = NULL;
+    dague_zpotrf_sp1dplus_handle_t *dague_zpotrf_sp = NULL;
 
-    dague_zpotrf_sp = dague_zpotrf_sp1dplus_new(A, (dague_ddesc_t *)A, NULL );
+    dague_zpotrf_sp = dague_zpotrf_sp1dplus_new( (dague_ddesc_t*)A, sopalin_data );
 
-    dague_zpotrf_sp->p_work = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t));
-    dague_private_memory_init( dague_zpotrf_sp->p_work, (A->pastix_data->solvmatr).coefmax * sizeof(dague_complex64_t) );
+    /* dague_zpotrf_sp->p_work = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t)); */
+    /* dague_private_memory_init( dague_zpotrf_sp->p_work, (A->pastix_data->solvmatr).coefmax * sizeof(dague_complex64_t) ); */
 
-
-    /* dsparse_add2arena_tile(((dague_zpotrf_Url_object_t*)dague_zpotrf)->arenas[DAGUE_zpotrf_Url_DEFAULT_ARENA],  */
+    /* dsparse_add2arena_tile(((dague_zpotrf_Url_handle_t*)dague_zpotrf)->arenas[DAGUE_zpotrf_Url_DEFAULT_ARENA],  */
     /*                        A->mb*A->nb*sizeof(dague_complex64_t), */
     /*                        DAGUE_ARENA_ALIGNMENT_SSE, */
     /*                        MPI_DOUBLE_COMPLEX, A->mb); */
 
-    return (dague_object_t*)dague_zpotrf_sp;
+    return (dague_handle_t*)dague_zpotrf_sp;
 }
 
 void
-dsparse_zpotrf_sp_Destruct( dague_object_t *o )
+dsparse_zpotrf_sp_Destruct( dague_handle_t *o )
 {
-    dague_zpotrf_sp1dplus_object_t *dague_zpotrf_sp = NULL;
-    dague_zpotrf_sp = (dague_zpotrf_sp1dplus_object_t *)o;
+    (void)o;
+    /* dague_zpotrf_sp1dplus_handle_t *dague_zpotrf_sp = NULL; */
+    /* dague_zpotrf_sp = (dague_zpotrf_sp1dplus_handle_t *)o; */
 
     /*dsparse_datatype_undefine_type( &(opotrf->arenas[DAGUE_zpotrf_Url_DEFAULT_ARENA]->opaque_dtt) );*/
 
-    dague_private_memory_fini( dague_zpotrf_sp->p_work );
-    free( dague_zpotrf_sp->p_work );
+    /* dague_private_memory_fini( dague_zpotrf_sp->p_work ); */
+    /* free( dague_zpotrf_sp->p_work ); */
 
-    DAGUE_INTERNAL_OBJECT_DESTRUCT(o);
+    //DAGUE_INTERNAL_HANDLE_DESTRUCT(o);
 }
 
-int dsparse_zpotrf_sp( dague_context_t *dague, sparse_matrix_desc_t *A)
+int dsparse_zpotrf_sp( dague_context_t *dague,
+                       sparse_matrix_desc_t *A,
+                       sopalin_data_t *sopalin_data )
 {
-    dague_object_t *dague_zpotrf_sp = NULL;
+    dague_handle_t *dague_zpotrf_sp = NULL;
     int info = 0;
 
-    dague_zpotrf_sp = dsparse_zpotrf_sp_New( A );
+    dague_zpotrf_sp = dsparse_zpotrf_sp_New( A, sopalin_data );
 
     if ( dague_zpotrf_sp != NULL )
     {
-        dague_enqueue( dague, (dague_object_t*)dague_zpotrf_sp);
-        dague_progress( dague );
+        dague_enqueue( dague, (dague_handle_t*)dague_zpotrf_sp);
+        dague_context_wait( dague );
         dsparse_zpotrf_sp_Destruct( dague_zpotrf_sp );
     }
     return info;
