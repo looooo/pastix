845661978102d6dccbd4ff43ba82e15bbb87b79d
Gregoire Pichon <gregoire.pichon@inria.fr>
2016-04-25 20:40:50 +0200

    update testing main routines and testing_zrradd

--- testing/testing_zmain.c
+++ testing/testing_zmain.c
@@ -9,9 +9,13 @@
 #include <pastix.h>
 #include <testing_zmain.h>
 
+
+int   IONE     = 1;
+int   ISEED[4] = {0,0,0,1};   /* initial seed for zlarnv() */
+
 int main (int argc, char **argv)
 {
-    int info;
+    int info = -1;
     char func[32];
 
     /* Check for number of arguments*/
@@ -22,6 +26,9 @@ int main (int argc, char **argv)
     }
 
     sscanf( argv[1], "%s", func   );
+
+    argc -= 2;
+    argv += 2;
 
     /*
      * LR operations
@@ -36,6 +43,8 @@ int main (int argc, char **argv)
         printf( "TESTING %s FAILED : incorrect number of arguments\n", func);
     } else if ( info == -2 ) {
         printf( "TESTING %s FAILED : not enough memory\n", func);
+    } else if ( info == -3 ) {
+        printf( "TESTING %s FAILED : invalid operation\n", func);
     }
 
     return EXIT_SUCCESS;
