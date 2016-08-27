c830662d4b654e7531973f6a5b230c45152f459f
Mathieu Faverge <mathieu.faverge@inria.fr>
2016-08-27 16:04:07 +0200

    Working 2D LU jdf (still a pb with the kernels)

--- sopalin/parsec/zpotrf_sp1dplus_wrapper.c
+++ sopalin/parsec/zpotrf_sp1dplus_wrapper.c
@@ -14,20 +14,20 @@
 #include "common.h"
 #include "solver.h"
 #include "sopalin_data.h"
-#include "parsec/zpotrf_sp1dplus.h"
+#include "parsec/zpotrf_sp2d.h"
 
 dague_handle_t*
 dsparse_zpotrf_sp_New( sparse_matrix_desc_t *A,
                        sopalin_data_t *sopalin_data )
 {
-    dague_zpotrf_sp1dplus_handle_t *dague_zpotrf_sp = NULL;
+    dague_zpotrf_sp2d_handle_t *dague_zpotrf_sp = NULL;
 
-    dague_zpotrf_sp = dague_zpotrf_sp1dplus_new( (dague_ddesc_t*)A, sopalin_data, NULL );
+    dague_zpotrf_sp = dague_zpotrf_sp2d_new( (dague_ddesc_t*)A, sopalin_data, NULL );
 
     dague_zpotrf_sp->p_work = (dague_memory_pool_t*)malloc(sizeof(dague_memory_pool_t));
     dague_private_memory_init( dague_zpotrf_sp->p_work, sopalin_data->solvmtx->gemmmax * sizeof(pastix_complex64_t) );
 
-    dague_matrix_add2arena_rect( dague_zpotrf_sp->arenas[DAGUE_zpotrf_sp1dplus_DEFAULT_ARENA],
+    dague_matrix_add2arena_rect( dague_zpotrf_sp->arenas[DAGUE_zpotrf_sp2d_DEFAULT_ARENA],
                                  dague_datatype_double_complex_t,
                                  /*sopalin_data->solvmtx->gemmmax*/ 1, 1, 1 );
 
@@ -37,10 +37,10 @@ dsparse_zpotrf_sp_New( sparse_matrix_desc_t *A,
 void
 dsparse_zpotrf_sp_Destruct( dague_handle_t *handle )
 {
-    dague_zpotrf_sp1dplus_handle_t *dague_zpotrf_sp = NULL;
-    dague_zpotrf_sp = (dague_zpotrf_sp1dplus_handle_t *)handle;
+    dague_zpotrf_sp2d_handle_t *dague_zpotrf_sp = NULL;
+    dague_zpotrf_sp = (dague_zpotrf_sp2d_handle_t *)handle;
 
-    dague_matrix_del2arena( dague_zpotrf_sp->arenas[DAGUE_zpotrf_sp1dplus_DEFAULT_ARENA] );
+    dague_matrix_del2arena( dague_zpotrf_sp->arenas[DAGUE_zpotrf_sp2d_DEFAULT_ARENA] );
 
     dague_private_memory_fini( dague_zpotrf_sp->p_work );
     free( dague_zpotrf_sp->p_work );
