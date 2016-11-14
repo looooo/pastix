26c953b5ca5c25f18a9c26b8f51edaebd7998d65
Mathieu Faverge <mathieu.faverge@inria.fr>
2016-11-14 17:48:11 -0500

    Fix validation script

--- test/validate_exapnd.sh
+++ test/validate_exapnd.sh
@@ -3,16 +3,16 @@
 fltname=$1
 
 rm -f *.sort
-for i in `ls -1 *.dat`;
+for i in `ls -1 expand_*.dat`;
 do
     sort $i > ${i}.sort
 done
 
-for i in 0 1
+for i in 0
 do
     for mtxtype in General Symmetric Hermitian
     do
-        basefile=${i}_CSC_0_${mtxtype}_${fltname}_sparse_cp.dat.sort
+        basefile=expand_${i}_CSC_0_${mtxtype}_${fltname}_sparse_cp.dat.sort
 
         if [ -f $basefile ]
         then
@@ -28,12 +28,40 @@ do
                         do
 
                             echo -n "---- CSC Sparse CP VS $fmttype $storage $comp $baseval: "
-                            diff $basefile ${i}_${fmttype}_${baseval}_${mtxtype}_${fltname}_${storage}_${comp}.dat.sort | wc -l
+                            diff $basefile expand_${i}_${fmttype}_${baseval}_${mtxtype}_${fltname}_${storage}_${comp}.dat.sort | wc -l
                         done
                     done
                 done
             done
         fi
+    done
+done
+
+for i in 1
+do
+    for mtxtype in General Symmetric Hermitian
+    do
+        for fmttype in CSC CSR IJV
+        do
+            basefile=expand_${i}_${fmttype}_0_${mtxtype}_${fltname}_sparse_cp.dat.sort
+
+            if [ -f $basefile ]
+            then
+                echo "-- Dof=$i, $mtxtype -- "
+
+                for baseval in 0 1
+                do
+                    for storage in sparse dense
+                    do
+                        for comp in cp ucp
+                        do
+                            echo -n "---- $fmtype Sparse CP VS $fmttype $storage $comp $baseval: "
+                            diff $basefile expand_${i}_${fmttype}_${baseval}_${mtxtype}_${fltname}_${storage}_${comp}.dat.sort | wc -l
+                        done
+                    done
+                done
+            fi
+        done
     done
 done
 
