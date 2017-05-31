27a76c3bf4cd825fecbd1b82d2a0865d6ced1d08
Mathieu Faverge <mathieu.faverge@inria.fr>
2017-05-31 11:25:44 -0400

    Remove attic directory

--- attic/examples/mem_trace.h
+++
@@ -1,5 +0,0 @@
-#ifndef MEM_TRACE_H
-#define MEM_TRACE_H
-void memtrace_start(void);
-void memtrace_stop(void);
-#endif
--- attic/starpu/starpu_kernels.h
+++
@@ -1,9 +0,0 @@
-#ifndef STARPU_KERNELS_H
-#define STARPU_KERNELS_H
-
-#include "redefine_functions.h"
-#include "sopalin_define.h"
-
-
-
-#endif /* STARPU_KERNELS_H */
