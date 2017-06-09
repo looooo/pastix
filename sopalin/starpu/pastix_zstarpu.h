2fd222515d97d133d6857a5bc2be0dd68a0602b7
Mathieu Faverge <mathieu.faverge@inria.fr>
2017-06-09 17:29:21 +0200

    Add codelet declaration

--- sopalin/starpu/pastix_zstarpu.h
+++ sopalin/starpu/pastix_zstarpu.h
@@ -34,6 +34,30 @@ starpu_task_cblk_zgemmsp( pastix_coefside_t sideA,
                           sopalin_data_t   *sopalin_data );
 
 void
+starpu_task_blok_zpotrf( sopalin_data_t *sopalin_data,
+                         SolverCblk     *cblk );
+
+void
+starpu_task_blok_ztrsmsp( pastix_coefside_t coef,
+                          pastix_side_t     side,
+                          pastix_uplo_t     uplo,
+                          pastix_trans_t    trans,
+                          pastix_diag_t     diag,
+                          const SolverCblk *cblk,
+                          SolverBlok       *blok,
+                          sopalin_data_t   *sopalin_data );
+
+void
+starpu_task_blok_zgemmsp( pastix_coefside_t sideA,
+                          pastix_coefside_t sideB,
+                          pastix_trans_t    trans,
+                          const SolverCblk *cblk,
+                          SolverCblk       *fcblk,
+                          const SolverBlok *blokA,
+                          const SolverBlok *blokB,
+                          sopalin_data_t   *sopalin_data );
+
+void
 starpu_zpotrf( pastix_data_t  *pastix_data,
                sopalin_data_t *sopalin_data );
 
