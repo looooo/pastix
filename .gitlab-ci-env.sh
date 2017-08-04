e82571b3b086588abdbd6fa19002d356a8339193
Pierre Ramet <ramet@labri.fr>
2017-08-04 14:47:53 +0200

    cleanup CI

--- .gitlab-ci-env.sh
+++ .gitlab-ci-env.sh
@@ -1,8 +1,7 @@
 #export version_cppcheck=1.79
 #export PATH=/builds/sonar/cppcheck-${version_cppcheck}/install/bin:$PATH
-export version_drmemory=1.11.0-2
-export PATH=/builds/sonar/DrMemory-Linux-${version_drmemory}/bin:$PATH
-#export PATH=/builds/sonar/sonar-scanner-2.8/bin:$PATH
+#export version_drmemory=1.11.0-2
+#export PATH=/builds/sonar/DrMemory-Linux-${version_drmemory}/bin:$PATH
 export PATH=/builds/sonar/sonar-scanner-2.9.0.670/bin:$PATH
 export PATH=/builds/sonar/lcov-to-cobertura-xml-1.6/lcov_cobertura:$PATH
 
@@ -13,6 +12,6 @@ export PKG_CONFIG_PATH=/builds/install/starpu/lib/pkgconfig:$PKG_CONFIG_PATH
 export PATH=/builds/install/parsec/bin:$PATH
 export LD_LIBRARY_PATH=/builds/install/parsec/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=/builds/install/parsec/lib/pkgconfig:$PKG_CONFIG_PATH
-#Scotc
+#Scotch
 export LD_LIBRARY_PATH=/builds/install/scotch/lib:$LD_LIBRARY_PATH
 export SCOTCH_DIR=/builds/install/scotch/
