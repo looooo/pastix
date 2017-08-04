a60ed592955873a7981c49d74e1ae9547cdfb1d9
Pierre Ramet <ramet@labri.fr>
2017-08-04 14:25:12 +0200

    clean CI

--- .gitlab-ci-env.sh
+++ .gitlab-ci-env.sh
@@ -4,15 +4,15 @@ export version_drmemory=1.11.0-2
 export PATH=/builds/sonar/DrMemory-Linux-${version_drmemory}/bin:$PATH
 #export PATH=/builds/sonar/sonar-scanner-2.8/bin:$PATH
 export PATH=/builds/sonar/sonar-scanner-2.9.0.670/bin:$PATH
-export COBERTURA_PATH=/builds/sonar/lcov-to-cobertura-xml-1.6/lcov_cobertura/
+export PATH=/builds/sonar/lcov-to-cobertura-xml-1.6/lcov_cobertura:$PATH
 
 #StarPU
-export LD_LIBRARY_PATH=/builds/tmp/starpu/install/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=/builds/tmp/starpu/install/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=/builds/install/starpu/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/builds/install/starpu/lib/pkgconfig:$PKG_CONFIG_PATH
 #PaRSEC
-export PATH=/builds/tmp/parsec/install/bin:$PATH
-export LD_LIBRARY_PATH=/builds/tmp/parsec/install/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=/builds/tmp/parsec/install/lib/pkgconfig:$PKG_CONFIG_PATH
+export PATH=/builds/install/parsec/bin:$PATH
+export LD_LIBRARY_PATH=/builds/install/parsec/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/builds/install/parsec/lib/pkgconfig:$PKG_CONFIG_PATH
 #Scotc
-export LD_LIBRARY_PATH=/builds/tmp/scotch/lib:$LD_LIBRARY_PATH
-export SCOTCH_DIR=/builds/tmp/scotch/
+export LD_LIBRARY_PATH=/builds/install/scotch/lib:$LD_LIBRARY_PATH
+export SCOTCH_DIR=/builds/install/scotch/
