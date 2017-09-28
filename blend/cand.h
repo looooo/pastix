9c08c889a434b20d5d17409b0699d3dfc1fb10ab
Mathieu Faverge <mathieu.faverge@inria.fr>
2017-09-28 11:00:19 +0200

    Fix indent

--- blend/cand.h
+++ blend/cand.h
@@ -42,12 +42,12 @@ int  candCheck( const Cand            *candtab,
 void candSave ( const Cand            *candtab,
                       pastix_int_t     cblknbr,
                       char           **directory );
-void candBuild          (       pastix_int_t           level_tasks2d,
-                                pastix_int_t           width_tasks2d,
-                                pastix_compress_when_t lr_when,
-                                pastix_int_t           lr_width,
-                      Cand            *candtab,
-                      EliminTree      *etree,
+void candBuild( pastix_int_t           level_tasks2d,
+                pastix_int_t           width_tasks2d,
+                pastix_compress_when_t lr_when,
+                pastix_int_t           lr_width,
+                Cand                  *candtab,
+                EliminTree            *etree,
                 const symbol_matrix_t *symbmtx,
                 const CostMatrix      *costmtx );
 
