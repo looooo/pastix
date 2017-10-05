938fb43297f7b474eb96b5f72903226cf299751f
Mathieu Faverge <mathieu.faverge@inria.fr>
2017-10-05 11:18:20 +0200

    Add missing files and remove old ones

---
+++ kernels/kernels_trace.c
@@ -0,0 +1,142 @@
+/**
+ *
+ * @file kernels_trace.c
+ *
+ *  PaStiX tarce and modelling routines
+ *  PaStiX is a software package provided by Inria Bordeaux - Sud-Ouest,
+ *  LaBRI, University of Bordeaux 1 and IPB.
+ *
+ * @version 5.1.0
+ * @author Mathieu Faverge
+ * @date 2017-02-09
+ *
+ **/
+#include "common.h"
+#include "solver.h"
+#include "kernels_trace.h"
+
+volatile double kernels_flops[PastixKernelLvl1Nbr];
+
+#if defined(PASTIX_WITH_EZTRACE)
+
+int pastix_eztrace_level = 1; /**< Level of kernel tracing with EZTrace */
+
+#endif
+
+#if defined(PASTIX_GENERATE_MODEL)
+
+pastix_model_entry_t *model_entries     = NULL;  /**< Array to all entries                 */
+volatile int32_t      model_entries_nbr = -1;    /**< Index of the last entry in the array */
+int32_t               model_size        = 0;     /**< Size of the model_entries array      */
+
+#endif
+
+void
+kernelsTraceStart( const SolverMatrix *solvmtx )
+{
+#if defined(PASTIX_WITH_EZTRACE)
+    {
+        char *level = pastix_getenv("PASTIX_EZTRACE_LEVEL");
+        if (level != NULL) {
+            pastix_eztrace_level = atoi(level);
+            pastix_cleanenv(level);
+        }
+        eztrace_start ();
+    }
+#endif /* defined(PASTIX_WITH_EZTRACE) */
+
+#if defined(PASTIX_GENERATE_MODEL)
+    {
+        pastix_int_t cblknbr   = solvmtx->cblknbr;
+        pastix_int_t cblkmin2d = solvmtx->cblkmin2d;
+        pastix_int_t total_number_of_tasks = 0;
+        pastix_int_t nbfact, nbtrsm, nbgemm;
+        pastix_int_t cblknum;
+        SolverCblk  *cblk;
+
+        /* Factorization kernels */
+        nbfact = cblknbr;
+
+        /* TRSM kernels */
+        nbtrsm = cblkmin2d + (cblknbr - cblkmin2d) * solvmtx->cblkmaxblk;
+        if ( solvmtx->factotype == PastixFactLU ) {
+            nbtrsm *= 2;
+        }
+
+        /* GEMM kernels */
+        nbgemm = solvmtx->bloknbr - cblknbr;
+        if ( solvmtx->factotype == PastixFactLU ) {
+            nbgemm *= 2;
+        }
+
+        cblk = solvmtx->cblktab+cblkmin2d;
+        for(cblknum = cblkmin2d; cblknum < cblknbr; cblknum++, cblk++ ) {
+            pastix_int_t nbodb = (cblk[1].fblokptr - cblk[0].fblokptr) - 1;
+
+            if ( solvmtx->factotype == PastixFactLU ) {
+                nbgemm += nbodb * nbodb;
+            }
+            else {
+                nbgemm += (nbodb * (nbodb-1)) / 2;
+            }
+        }
+
+        total_number_of_tasks = nbfact + nbtrsm + nbgemm;
+        model_entries = malloc( total_number_of_tasks * sizeof(pastix_model_entry_t) );
+        model_size = total_number_of_tasks;
+    }
+#endif
+
+    memset( (void*)kernels_flops, 0, PastixKernelLvl1Nbr * sizeof(double) );
+
+    return;
+}
+
+double
+kernelsTraceStop( char *directory )
+{
+    double total_flops = 0.0;
+
+#if defined(PASTIX_WITH_EZTRACE)
+    eztrace_stop ();
+#endif
+
+#if defined(PASTIX_GENERATE_MODEL)
+    {
+        pastix_model_entry_t *entry = model_entries;
+        pastix_int_t i;
+        FILE *f;
+
+        f = fopen( "model.csv", "a" );
+        if ( f != NULL )
+        {
+            for(i=0; i <= model_entries_nbr; i++, entry++ ) {
+                fprintf( f, "%d;%d;%d;%d;%e\n",
+                         entry->ktype, entry->m, entry->n, entry->k, entry->time );
+            }
+
+            fclose( f );
+        }
+
+        free( model_entries );
+
+        /* Reinitialize values */
+        model_entries     = NULL;
+        model_entries_nbr = -1;
+        model_size        = 0;
+    }
+#endif
+
+    {
+        int i;
+
+        /* Compute the total number of flops */
+        for( i=0; i<PastixKernelLvl1Nbr; i++ )
+        {
+            total_flops += kernels_flops[i];
+        }
+    }
+    (void)directory;
+
+    return total_flops;
+}
---
+++ kernels/kernels_trace.h
@@ -0,0 +1,236 @@
+/**
+ *
+ * @file kernels_trace.h
+ *
+ * Wrappers to trace kernels.
+ *
+ * @copyright 2004-2017 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
+ *                      Univ. Bordeaux. All rights reserved.
+ *
+ * @version 6.0.0
+ * @author Gregoire Pichon
+ * @date 2017-04-26
+ *
+ * @addtogroup eztrace_dev
+ * @{
+ *
+ **/
+
+#ifndef _kernels_trace_h_
+#define _kernels_trace_h_
+
+#include "common.h"
+#include "flops.h"
+
+#define PastixKernelStop 0
+
+typedef enum pastix_ktype0_e {
+    PastixKernelLvl0Facto,
+    PastixKernelLvl0Solve,
+    PastixKernelLvl0Diag,
+    PastixKernelLvl0Nbr
+} pastix_ktype0_t;
+
+typedef enum pastix_ktype_e {
+    PastixKernelGETRF,
+    PastixKernelHETRF,
+    PastixKernelPOTRF,
+    PastixKernelPXTRF,
+    PastixKernelSYTRF,
+    PastixKernelSCALOCblk,
+    PastixKernelSCALOBlok,
+    PastixKernelTRSMCblk1d,
+    PastixKernelTRSMCblk2d,
+    PastixKernelTRSMCblkLR,
+    PastixKernelTRSMBlok2d,
+    PastixKernelTRSMBlokLR,
+    PastixKernelGEMMCblk1d1d,
+    PastixKernelGEMMCblk1d2d,
+    PastixKernelGEMMCblk2d2d,
+    PastixKernelGEMMCblkFRLR,
+    PastixKernelGEMMCblkLRLR,
+    PastixKernelGEMMBlok2d2d,
+    PastixKernelGEMMBlok2dLR,
+    PastixKernelLvl1Nbr
+} pastix_ktype_t;
+
+typedef enum pastix_ktype2_e {
+
+    /* General kernels: similar in low-rank and dense */
+    PastixKernelLvl2GETRF,
+    PastixKernelLvl2HETRF,
+    PastixKernelLvl2POTRF,
+    PastixKernelLvl2PXTRF,
+    PastixKernelLvl2SYTRF,
+
+    /* Dense operations */
+    PastixKernelLvl2_FR_TRSM, /**< trsm on a dense block           */
+    PastixKernelLvl2_FR_GEMM, /**< gemm between three dense blocks */
+
+    /* Low-rank operations */
+    PastixKernelLvl2_LR_INIT,          /**< try to compress a dense block (RRQR)                  */
+    PastixKernelLvl2_LR_INIT_Q,        /**< form Q when compression succeeded                     */
+    PastixKernelLvl2_LR_TRSM,          /**< trsm on a low-rank block                              */
+    PastixKernelLvl2_LR_GEMM_PRODUCT,  /**< formation of a product of low-rank blocks             */
+    PastixKernelLvl2_LR_GEMM_ADD_Q,    /**< getrf/unmqr during recompression                      */
+    PastixKernelLvl2_LR_GEMM_ADD_RRQR, /**< compression of concatenated matrices in recompression */
+    PastixKernelLvl2_LR_UNCOMPRESS,    /**< uncompress a low-rank block into a dense block        */
+
+    PastixKernelLvl2Nbr
+} pastix_ktype2_t;
+
+extern volatile double kernels_flops[PastixKernelLvl1Nbr];
+
+#define PastixKernelsNbr (PastixKernelLvl0Nbr + PastixKernelLvl1Nbr + PastixKernelLvl2Nbr)
+
+#if defined(PASTIX_WITH_EZTRACE)
+
+#include "eztrace_module/kernels_ev_codes.h"
+
+extern int pastix_eztrace_level;
+
+#else
+
+static inline void kernel_trace_start_lvl0( pastix_ktype0_t ktype ) { (void)ktype; };
+static inline void kernel_trace_stop_lvl0 ( double flops ) { (void)flops; };
+static inline void kernel_trace_start_lvl2( pastix_ktype2_t ktype ) { (void)ktype; };
+static inline void kernel_trace_stop_lvl2 ( double flops ) { (void)flops; };
+
+#endif
+
+#if defined(PASTIX_GENERATE_MODEL)
+
+typedef struct pastix_model_entry_s {
+    pastix_ktype_t ktype;
+    int m;
+    int n;
+    int k;
+    double time;
+} pastix_model_entry_t;
+
+extern pastix_model_entry_t *model_entries;     /*< Array to all entries                 */
+extern volatile int32_t      model_entries_nbr; /*< Index of the last entry in the array */
+extern int32_t               model_size;        /*< Allocated size of the array          */
+
+#endif
+
+void   kernelsTraceStart( const SolverMatrix *solvmtx );
+double kernelsTraceStop( char *directory );
+
+/**
+ *******************************************************************************
+ *
+ * @brief Start the trace of a single kernel
+ *
+ *******************************************************************************
+ *
+ * @param[in] ktype
+ *          Type of the kernel starting that need to be traced.
+ *          With EZTrace mode, this call is empty if the environment variable
+ *          PASTIX_EZTRACE_LEVEL is different from 1.
+ *
+ *******************************************************************************
+ *
+ * @return the starting time if PASTIX_GENERATE_MODEL is enabled, 0. otherwise.
+ *
+ *******************************************************************************/
+static inline double
+kernel_trace_start( pastix_ktype_t ktype )
+{
+    double time = 0.;
+
+#if defined(PASTIX_WITH_EZTRACE)
+
+    if (pastix_eztrace_level == 1) {
+        EZTRACE_EVENT_PACKED_0( KERNELS_LVL1_CODE(ktype) );
+    }
+
+#endif
+
+#if defined(PASTIX_GENERATE_MODEL)
+
+    time = clockGet();
+
+#endif
+
+    (void)ktype;
+    return time;
+}
+
+/**
+ *******************************************************************************
+ *
+ * @brief Stop the trace of a single kernel
+ *
+ *******************************************************************************
+ *
+ * @param[in] ktype
+ *          Type of the kernel starting that need to be traced.
+ *          With EZTrace mode, this call is empty if the environment variable
+ *          PASTIX_EZTRACE_LEVEL is different from 1.
+ *
+ * @param[in] m
+ *          The m parameter of the kernel (used by xxTRF, TRSM, and GEMM)
+ *
+ * @param[in] n
+ *          The n parameter of the kernel (used by TRSM, and GEMM)
+ *
+ * @param[in] k
+ *          The k parameter of the kernel (used by GEMM)
+ *
+ * @param[in] flops
+ *          The number of flops of the kernel
+ *
+ * @param[in] starttime
+ *          The stating time of the kernel. Used only if PASTIX_GENERATE_MODEL
+ *          is enabled.
+ *
+ *******************************************************************************/
+static inline void
+kernel_trace_stop( pastix_ktype_t ktype, int m, int n, int k, double flops, double starttime )
+{
+
+#if defined(PASTIX_WITH_EZTRACE)
+
+    if (pastix_eztrace_level == 1) {
+        EZTRACE_EVENT_PACKED_1( KERNELS_CODE( PastixKernelStop ), flops );
+    }
+
+#endif
+
+#if defined(PASTIX_GENERATE_MODEL)
+
+    {
+        double  time = clockGet() - starttime;
+        int32_t index = pastix_atomic_inc_32b( &model_entries_nbr );
+
+        if ( index < model_size ) {
+            model_entries[index].ktype = ktype;
+            model_entries[index].m     = m;
+            model_entries[index].n     = n;
+            model_entries[index].k     = k;
+            model_entries[index].time  = time;
+        }
+        else {
+            fprintf(stderr, "WARNING: too many kernels to log %d\n", index);
+        }
+    }
+
+#endif
+
+    /* { */
+    /*     double oldval, newval; */
+    /*     do { */
+    /*         oldval = kernels_flops[ktype]; */
+    /*         newval = oldval + flops; */
+    /*     } while( !pastix_atomic_cas_64b( (uint64_t*)(kernels_flops + ktype), oldval, newval ) ); */
+    /* } */
+
+    (void)m;
+    (void)n;
+    (void)k;
+    (void)starttime;
+    return;
+}
+
+#endif/* _kernels_trace_h_ */
