a9ebabd0b107dbf224fd4882a80da8f106bed326
Mathieu Faverge <mathieu.faverge@inria.fr>
2017-10-05 13:49:11 +0200

    Cleanup output of statistics module

--- kernels/eztrace_module/eztrace_convert_kernels.c
+++ kernels/eztrace_module/eztrace_convert_kernels.c
@@ -9,66 +9,45 @@
  *
  * @version 6.0.0
  * @author Gregoire Pichon
+ * @author Mathieu Faverge
  * @date 2017-04-26
  *
  */
 #define _GNU_SOURCE 1
-#include "eztrace_convert_kernels.h"
+#include <stdio.h>
+#include <strings.h>
+#include <GTG.h>
+#include "eztrace_convert.h"
+#include "kernels_trace.h"
 
-struct eztrace_convert_module kernels_module;
+static inline double kernels_dmin( double a, double b) {
+    return ( a < b ) ? a : b;
+}
 
-static kernels_t kernels_properties[PastixKernelsNbr];
-
-/**
- *******************************************************************************
- *
- * @brief Initialize data associated with each kernel for a thread
- *
- *******************************************************************************
- *
- * @param[in] p_thread
- *          The reference to the thread managed by eztrace
- *
- * @param[in] stats
- *          - stats == 0, save event
- *          - stats == 1, save statistics
- *
- *******************************************************************************
- *
- * @return  The structure containing the thread and statistics for each kernel
- *
- *******************************************************************************/
-static kernels_thread_info_t *
-kernels_register_thread_hook( struct thread_info_t *p_thread,
-                              int stats )
-{
-    kernels_thread_info_t *p_info = (kernels_thread_info_t*) malloc(sizeof(kernels_thread_info_t));
-
-    p_info->p_thread = p_thread;
-
-    if (stats == 1){
-        int    *nb       = malloc(PastixKernelsNbr * sizeof(int));
-        double *flops    = malloc(PastixKernelsNbr * sizeof(double));
-        double *run_time = malloc(PastixKernelsNbr * sizeof(double));
-
-        int i;
-        for (i=0; i<PastixKernelsNbr; i++){
-            nb[i]        = 0;
-            flops[i]     = 0;
-            run_time[i]  = 0;
-        }
-
-        p_info->nb       = nb;
-        p_info->flops    = flops;
-        p_info->run_time = run_time;
-    }
-
-    ezt_hook_list_add(&p_info->p_thread->hooks, p_info, KERNELS_EVENTS_ID);
-    return p_info;
+static inline double kernels_dmax( double a, double b) {
+    return ( a > b ) ? a : b;
 }
 
 /**
- * @brief Define events properties such as name or color
+ * @brief EZTrace kernels module structure
+ */
+struct eztrace_convert_module kernels_module;
+
+/**
+ * @brief Kernels basic information structure for traces
+ */
+typedef struct kernels_s {
+    char        *name;   /**< Name of the kernel in the traces          */
+    gtg_color_t  color;  /**< Default color of the kernel in the traces */
+} kernels_t;
+
+/**
+ * @brief Table of kernels default names and colors
+ */
+static kernels_t kernels_properties[PastixKernelsNbr];
+
+/**
+ * @brief Initialize the kernels_properties table
  */
 void
 define_kernels_properties()
@@ -125,6 +104,79 @@ define_kernels_properties()
     kernels_lvl2[PastixKernelLvl2_LR_UNCOMPRESS]    = (kernels_t) {"lvl2_lr_uncompress",    GTG_PINK      };
 }
 
+
+typedef enum counter_e {
+    CounterMin,
+    CounterMax,
+    CounterSum,
+    CounterSum2
+} counter_t;
+
+/**
+ * @brief Information structure associated to one kernel on one thread to
+ * compute statistics.
+ */
+typedef struct kernels_info_s {
+    int    nb;      /**< Number of calls to the kernel       */
+    double flop[4]; /**< Flops counters for statistics       */
+    double time[4]; /**< Timing counters for statistics      */
+    double perf[4]; /**< Performance counters for statistics */
+} kernels_info_t;
+
+/**
+ * @brief Information structure associated to each thread to store temporary
+ * information between two events while computing statistics.
+ */
+typedef struct kernels_thread_info_s {
+    struct thread_info_t *p_thread;   /**< Common EZTrace thread information                                 */
+    float                 time_start; /**< Sarting time of the last non PastixKernelStop event               */
+    int                   current_ev; /**< Id of the last non PastixKernelStop event                         */
+    kernels_info_t       *kernels;    /**< Table of size PastixKernelsNbr to store statistics on each kernel */
+} kernels_thread_info_t;
+
+#define INIT_KERNELS_THREAD_INFO(p_thread, var, stats)                  \
+    kernels_thread_info_t *var = (kernels_thread_info_t *)              \
+        ezt_hook_list_retrieve_data(&p_thread->hooks, KERNELS_EVENTS_ID); \
+    if(!(var)) {                                                        \
+        var = kernels_register_thread_hook(p_thread, stats);            \
+    }
+
+/**
+ *******************************************************************************
+ *
+ * @brief Initialize data associated with each kernel for a thread
+ *
+ *******************************************************************************
+ *
+ * @param[in] p_thread
+ *          The reference to the thread managed by eztrace
+ *
+ * @param[in] stats
+ *          - stats == 0, save event
+ *          - stats == 1, save statistics
+ *
+ *******************************************************************************
+ *
+ * @return  The structure containing the thread and statistics for each kernel
+ *
+ *******************************************************************************/
+static kernels_thread_info_t *
+kernels_register_thread_hook( struct thread_info_t *p_thread,
+                              int stats )
+{
+    kernels_thread_info_t *p_info = (kernels_thread_info_t*) malloc(sizeof(kernels_thread_info_t));
+
+    p_info->p_thread = p_thread;
+
+    if (stats == 1){
+        kernels_info_t *kernels = calloc( PastixKernelsNbr, sizeof(kernels_info_t) );
+        p_info->kernels = kernels;
+    }
+
+    ezt_hook_list_add(&p_info->p_thread->hooks, p_info, KERNELS_EVENTS_ID);
+    return p_info;
+}
+
 /**
  *******************************************************************************
  *
@@ -168,7 +220,7 @@ eztrace_convert_kernels_init()
  *          - stats == 1, save statistics
  *
  *******************************************************************************/
-void
+static inline void
 handle_start( int event_id, int stats )
 {
     DECLARE_THREAD_ID_STR(thread_id, CUR_INDEX, CUR_THREAD_ID);
@@ -177,7 +229,7 @@ handle_start( int event_id, int stats )
 
     if (stats == 1)
     {
-        p_info->nb[event_id]++;
+        p_info->kernels[event_id].nb ++;
         p_info->current_ev = event_id;
         p_info->time_start = CURRENT;
     }
@@ -198,19 +250,36 @@ handle_start( int event_id, int stats )
  *          - stats == 1, save statistics
  *
  *******************************************************************************/
-void
+static inline void
 handle_stop( int stats )
 {
-    double size;
-
     DECLARE_THREAD_ID_STR(thread_id, CUR_INDEX, CUR_THREAD_ID);
     DECLARE_CUR_THREAD(p_thread);
     INIT_KERNELS_THREAD_INFO(p_thread, p_info, stats);
 
-    if (stats == 1){
-        GET_PARAM_PACKED_1(CUR_EV, size);
-        p_info->run_time[p_info->current_ev] += (CURRENT - p_info->time_start);
-        p_info->flops[p_info->current_ev]    += size;
+    if (stats == 1)
+    {
+        kernels_info_t *kernel = p_info->kernels + p_info->current_ev;
+        double flop, time, perf;
+
+        GET_PARAM_PACKED_1(CUR_EV, flop);
+        flop = flop * 1.e-9;
+        kernel->flop[CounterMin ]  = kernels_dmin( kernel->flop[CounterMin], flop );
+        kernel->flop[CounterMax ]  = kernels_dmax( kernel->flop[CounterMax], flop );
+        kernel->flop[CounterSum ] += flop;
+        kernel->flop[CounterSum2] += flop * flop;
+
+        time = CURRENT - p_info->time_start;
+        kernel->time[CounterMin ]  = kernels_dmin( kernel->time[CounterMin], time );
+        kernel->time[CounterMax ]  = kernels_dmax( kernel->time[CounterMax], time );
+        kernel->time[CounterSum ] += time;
+        kernel->time[CounterSum2] += time * time;
+
+        perf = flop / (time * 1.e-3);
+        kernel->perf[CounterMin ]  = kernels_dmin( kernel->perf[CounterMin], perf );
+        kernel->perf[CounterMax ]  = kernels_dmax( kernel->perf[CounterMax], perf );
+        kernel->perf[CounterSum ] += perf;
+        kernel->perf[CounterSum2] += perf * perf;
     }
     else{
         popState(CURRENT, "ST_Thread", thread_id);
@@ -299,22 +368,28 @@ int handle_kernels_stats(eztrace_event_t *ev)
 /**
  * @brief Print the statistics of each kernel for each thread
  */
-void print_kernels_stats()
+void
+print_kernels_stats()
 {
-    int64_t i, j, k;
-    double total_flops = 0;
+    kernels_info_t total;
+    double *data;
+    int i, j, k;
+    int main_header   = 1;
+    int thread_header = 1;
 
     define_kernels_properties();
+    memset( &total, 0, sizeof(kernels_info_t) );
 
     /* Browse the list of processes */
-    for (i = 0; i < NB_TRACES; i++) {
+    for (i=0; i<NB_TRACES; i++) {
         struct eztrace_container_t *p_process = GET_PROCESS_CONTAINER(i);
 
         /* For each process, browse the list of threads */
-        for(j=0; j<(int64_t)(p_process->nb_children); j++) {
+        for(j=0; j<(int)(p_process->nb_children); j++) {
 
             struct eztrace_container_t *thread_container;
             struct thread_info_t       *p_thread;
+            kernels_info_t *kernel;
 
             thread_container = p_process->children[j];
             p_thread = (struct thread_info_t*)(thread_container->container_info);
@@ -324,28 +399,96 @@ void print_kernels_stats()
             }
 
             INIT_KERNELS_THREAD_INFO(p_thread, p_info, 1);
-            printf("\tThread %20s\n", thread_container->name);
 
-            for (k=0; k<PastixKernelsNbr; k++)
+            thread_header = 1;
+            kernel = p_info->kernels;
+            for (k=0; k<PastixKernelsNbr; k++, kernel++)
             {
-                if (p_info->nb[k] > 0)
+                if (kernel->nb > 0)
                 {
-                    double flops = p_info->flops[k];
-                    double time  = p_info->run_time[k] * 1.e-3;
-                    double perf  = flops / time;
+                    if ( main_header ) {
+                        printf( "         %20s | Nb calls | %-50s | %-50s | %-37s |\n"
+                                "         %20s |          | %12s %8s %8s %8s %10s | %12s %8s %8s %8s %10s | %8s %8s %8s %10s |\n",
+                                "", "Number of flop (GFlop)", "Timing (s)", "Performance (GFlop/s)",
+                                "", "total", "min", "max", "avg", "sd",
+                                    "total", "min", "max", "avg", "sd",
+                                             "min", "max", "avg", "sd" );
+                        main_header = 0;
+                    }
 
-                    total_flops += flops;
+                    if ( thread_header ) {
+                        printf( "  Thread %20s |\n", thread_container->name );
+                        thread_header = 0;
+                    }
 
-                    printf( "Kernel %-20s was called %8d times, flops=%8.3g, time=%7.2g s, perf=%7.2lf %cFlop/s\n",
-                            kernels_properties[k].name, p_info->nb[k],
-                            flops, time,
-                            printflopsv( perf ), printflopsu( perf ) );
+                    printf( "      %-23s | %8d |",
+                            kernels_properties[k].name, kernel->nb );
+
+                    data = kernel->flop;
+                    printf( " %e %8.3g %8.3g %8.3g +-%8.3g |",
+                            data[CounterSum], data[CounterMin], data[CounterMax],
+                            data[CounterSum] / (double)kernel->nb,
+                            sqrt( (data[CounterSum2] - (data[CounterSum] * data[CounterSum]) / (double)kernel->nb) / (double)kernel->nb ) );
+
+                    data = kernel->time;
+                    printf( " %e %8.3g %8.3g %8.3g +-%8.3g |",
+                            data[CounterSum], data[CounterMin], data[CounterMax],
+                            data[CounterSum] / (double)kernel->nb,
+                            sqrt( (data[CounterSum2] - (data[CounterSum] * data[CounterSum]) / (double)kernel->nb) / (double)kernel->nb ) );
+
+                    data = kernel->perf;
+                    printf( " %8.3g %8.3g %8.3g +-%8.3g |\n",
+                            data[CounterMin], data[CounterMax],
+                            data[CounterSum] / (double)kernel->nb,
+                            sqrt( (data[CounterSum2] - (data[CounterSum] * data[CounterSum]) / (double)kernel->nb) / (double)kernel->nb ) );
+
+                    total.nb += kernel->nb;
+
+                    total.flop[CounterMin ]  = kernels_dmin( total.flop[CounterMin], kernel->flop[CounterMin] );
+                    total.flop[CounterMax ]  = kernels_dmax( total.flop[CounterMax], kernel->flop[CounterMax] );
+                    total.flop[CounterSum ] += kernel->flop[CounterSum ];
+                    total.flop[CounterSum2] += kernel->flop[CounterSum2];
+
+                    total.time[CounterMin ]  = kernels_dmin( total.time[CounterMin], kernel->time[CounterMin] );
+                    total.time[CounterMax ]  = kernels_dmax( total.time[CounterMax], kernel->time[CounterMax] );
+                    total.time[CounterSum ] += kernel->time[CounterSum ];
+                    total.time[CounterSum2] += kernel->time[CounterSum2];
+
+                    total.perf[CounterMin ]  = kernels_dmin( total.perf[CounterMin], kernel->perf[CounterMin] );
+                    total.perf[CounterMax ]  = kernels_dmax( total.perf[CounterMax], kernel->perf[CounterMax] );
+                    total.perf[CounterSum ] += kernel->perf[CounterSum ];
+                    total.perf[CounterSum2] += kernel->perf[CounterSum2];
                 }
+            }
+            if ( !thread_header ) {
+                printf("\n");
             }
         }
     }
-    printf( "\n\n\tTotal number of operations: %5.2lf %cFlops\n",
-            printflopsv( total_flops ), printflopsu( total_flops ) );
+
+    if ( total.nb > 0 ) {
+        printf( "  Total  %20s |\n"
+                "      %-23s | %8d |",
+                "", "", total.nb );
+
+        data = total.flop;
+        printf( " %e %8.3g %8.3g %8.3g +-%8.3g |",
+                data[CounterSum], data[CounterMin], data[CounterMax],
+                data[CounterSum] / (double)total.nb,
+                sqrt( (data[CounterSum2] - (data[CounterSum] * data[CounterSum]) / (double)total.nb) / (double)total.nb ) );
+
+        data = total.time;
+        printf( " %e %8.3g %8.3g %8.3g +-%8.3g |",
+                data[CounterSum], data[CounterMin], data[CounterMax],
+                            data[CounterSum] / (double)total.nb,
+                sqrt( (data[CounterSum2] - (data[CounterSum] * data[CounterSum]) / (double)total.nb) / (double)total.nb ) );
+
+        data = total.perf;
+        printf( " %8.3g %8.3g %8.3g +-%8.3g |",
+                data[CounterMin], data[CounterMax],
+                data[CounterSum] / (double)total.nb,
+                sqrt( (data[CounterSum2] - (data[CounterSum] * data[CounterSum]) / (double)total.nb) / (double)total.nb ) );
+    }
 }
 
 /**
