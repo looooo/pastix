d0cb1d3702cb85576486f8eab26fda9304c763e3
Pierre Ramet <ramet@labri.fr>
2017-11-10 11:36:21 +0100

    fix error when atomic not supported

--- common/sys/atomic-xlc.h
+++ common/sys/atomic-xlc.h
@@ -3,6 +3,7 @@
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  */
+#include <stdio.h>
 
 static inline void pastix_mfence( void )
 {
@@ -10,22 +11,22 @@ static inline void pastix_mfence( void )
 }
 
 static inline int pastix_atomic_bor_32b( volatile uint32_t* location,
-                                        uint32_t value )
+                                         uint32_t value )
 {
     uint32_t old_value = __fetch_and_or(location, value);
     return old_value | value;
 }
 
 static inline int pastix_atomic_band_32b( volatile uint32_t* location,
-                                         uint32_t value )
+                                          uint32_t value )
 {
     uint32_t old_value = __fetch_and_and(location, value);
     return old_value & value;
 }
 
 static inline int pastix_atomic_cas_32b( volatile uint32_t* location,
-                                        uint32_t old_value,
-                                        uint32_t new_value )
+                                         uint32_t old_value,
+                                         uint32_t new_value )
 {
     int32_t old = (int32_t)old_value;
     return __compare_and_swap( (volatile int*)location, &old, new_value );
@@ -33,19 +34,18 @@ static inline int pastix_atomic_cas_32b( volatile uint32_t* location,
 
 #if defined(HAVE_ATOMIC_XLC_64_BUILTINS)
 static inline int pastix_atomic_cas_64b( volatile uint64_t* location,
-                                        uint64_t old_value,
-                                        uint64_t new_value )
+                                         uint64_t old_value,
+                                         uint64_t new_value )
 {
     int64_t old = (int64_t)old_value;
     return __compare_and_swaplp( (volatile long*)location, &old, new_value );
 }
 #else
-#include "debug.h"
 static inline int pastix_atomic_cas_64b( volatile uint64_t* location,
-                                        uint64_t old_value,
-                                        uint64_t new_value )
+                                         uint64_t old_value,
+                                         uint64_t new_value )
 {
-    ERROR(("Use of 64b CAS using atomic-xlc without compiler support\n \n"));
+    fprintf(stderr, "Use of 64b CAS using atomic-xlc without compiler support\n \n");
     return -1;
 }
 #endif
