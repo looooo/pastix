64106a49f7ed243219a4e246247ecd0c2cc4eb80
Vincent Bridonneau <vincent.bridonneau@inria.fr>
2018-06-08 14:40:48 +0200

    Move to defitions into a new header file.

--- symbol/symbol.h
+++ symbol/symbol.h
@@ -122,8 +122,6 @@ void         pastixSymbolGetTimes  ( const symbol_matrix_t *symbmtx,
  * @name Symbol reordering subroutines
  * @{
  */
-void symbol_reorder_cblk( const symbol_matrix_t *symbptr, const symbol_cblk_t *cblk, pastix_order_t *order, const pastix_int_t *levels, pastix_int_t *depthweight, pastix_int_t  depthmax, pastix_int_t  split_level, pastix_int_t  stop_criteria );
-void symbol_reorder( pastix_data_t *pastix_data, pastix_int_t maxdepth, pastix_int_t *levels );
 void pastixSymbolReordering( pastix_data_t* );
 void pastixSymbolReorderingPrintComplexity( const symbol_matrix_t *symbptr );
 
