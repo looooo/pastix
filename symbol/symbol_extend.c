5a56f3193267cbc693aaa6db7f9db116ff81c882
Vincent Bridonneau <vincent.bridonneau@inria.fr>
2018-07-30 15:31:43 +0200

    Implement expansion of symbol's structure (both constant and non-constant dof).

--- symbol/symbol_extend.c
+++ symbol/symbol_extend.c
@@ -39,6 +39,154 @@ __find_cblk_schurfcol( symbol_matrix_t *symbptr )
     return NULL;
 }
 
+static inline void
+__update_lcolnum( symbol_cblk_t *cblk,
+                  pastix_int_t  *dofs,
+                  pastix_int_t  *rangtab,
+                  pastix_int_t  *peritab,
+                  pastix_int_t   cblknum )
+{
+    pastix_int_t sum, i, begin, end;
+
+    begin = rangtab[cblknum];
+    end   = rangtab[cblknum + 1];
+    sum   = 0;
+    for (i = begin; i < end; ++i)
+    {
+        sum += dofs[peritab[i] + 1] - dofs[peritab[i]];
+    }
+
+    cblk->lcolnum = sum + cblk->fcolnum;
+}
+
+/* Should be call after update cblks */
+static inline void
+__update_bloks_in_cblk( symbol_blok_t *blok,     /* First blok in a cblk */
+                        symbol_cblk_t *cblktabu, /* Updated */
+                        symbol_cblk_t *cblktabp, /* Not updated */
+                        pastix_int_t  *dofs,
+                        pastix_int_t  *peritab )
+{
+    pastix_int_t   begin, end, sum, i;
+    pastix_int_t   cblknum;
+    symbol_cblk_t *fcblku, *fcblkp;
+    pastix_int_t   rg_init, rg_end, j;
+
+    cblknum = blok->lcblknm;
+    begin   = cblktabp[cblknum].bloknum;
+    end     = cblktabp[cblknum + 1].bloknum;
+
+    for (i = begin; i < end; ++i, ++blok)
+    {
+        fcblku  = cblktabu + blok->fcblknm; /* Updated */
+        fcblkp  = cblktabp + blok->fcblknm; /* Not updated */
+        rg_init = fcblkp->fcolnum;
+        rg_end  = blok->frownum;
+        sum     = 0;
+
+        /* First : compute the frownum */
+        for (j = rg_init; j < rg_end; ++j)
+        {
+            sum += dofs[peritab[j] + 1] - dofs[peritab[j]];
+        }
+        blok->frownum = fcblku->fcolnum + sum;
+        rg_end = blok->lrownum;
+
+        /* Then : compute the lrownum */
+        for (; j <= rg_end; ++j)
+        {
+            sum += dofs[peritab[j] + 1] - dofs[peritab[j]];
+        }
+        blok->lrownum = fcblku->fcolnum + sum;
+    }
+}
+
+static inline void
+__extend_dof_cte(       symbol_matrix_t *symbptr,
+                  const pastix_order_t  *ordeptr )
+{
+    pastix_int_t   col, cblknbr, bloknbr, row, dof;
+    symbol_cblk_t *cblk;
+    symbol_blok_t *blok;
+
+    dof     = symbptr->dof;
+    cblk    = symbptr->cblktab;
+    cblknbr = symbptr->cblknbr;
+
+    /* Update cblks first */
+    for (col = 0; col <= cblknbr; ++col)
+    {
+        cblk[col].fcolnum *= dof;
+        cblk[col].lcolnum *= dof;
+    }
+
+    bloknbr = symbptr->bloknbr;
+    blok    = symbptr->bloktab;
+    /* Update block row and column indexes */
+    for (row = 0; row < bloknbr; ++row)
+    {
+        blok[row].frownum *= dof;
+        blok[row].lrownum *= dof;
+    }
+
+    symbptr->nodenbr   *= dof;
+    symbptr->schurfcol *= dof;
+
+    (void)ordeptr;
+}
+
+static inline void
+__extend_dof_not_cte(       symbol_matrix_t *symbptr,
+                      const pastix_order_t  *ordeptr )
+{
+    pastix_int_t   col, cblknbr;
+    symbol_cblk_t *cblk, *schurcblk, *cblktab_cpy;
+    symbol_blok_t *blok;
+    pastix_int_t  *rangtab, *peritab, *dofs;
+
+    dofs    = symbptr->dofs;
+    cblknbr = symbptr->cblknbr;
+    cblk    = symbptr->cblktab;
+    peritab = ordeptr->peritab;
+    rangtab = ordeptr->rangtab;
+
+    schurcblk = __find_cblk_schurfcol( symbptr );
+
+    /*
+     * Make a copy of cblkatb to have both updated values (to compute (f/l)rows)
+     * And previous ones (to have start indexes for bloks)
+     */
+    MALLOC_INTERN( cblktab_cpy, cblknbr + 1, symbol_cblk_t );
+    memcpy ( cblktab_cpy, cblk, (cblknbr + 1) * sizeof(symbol_cblk_t) );
+
+    /* Update cblks first */
+    cblk[0].fcolnum = 0;
+    __update_lcolnum( cblk, dofs, rangtab, peritab, 0 );
+
+    for (col = 1; col < cblknbr; ++col)
+    {
+        cblk[col].fcolnum = cblk[col - 1].lcolnum + 1;
+        __update_lcolnum( cblk + col, dofs, rangtab, peritab, col );
+    }
+
+    /* Update last column blok */
+    cblk[col].fcolnum  = cblk[col - 1].lcolnum + 1;
+    cblk[col].lcolnum  = cblk[col].fcolnum;
+
+    blok    = symbptr->bloktab;
+    /* Udpate bloks according to cblks */
+    for (col = 0; col < cblknbr; ++col)
+    {
+        __update_bloks_in_cblk( blok + cblk[col].bloknum,
+                                cblk, cblktab_cpy, dofs, peritab );
+    }
+
+    symbptr->schurfcol = schurcblk->fcolnum;
+    symbptr->nodenbr   = cblk[symbptr->cblknbr].lcolnum;
+
+    memFree_null( cblktab_cpy );
+}
+
 /**
  *******************************************************************************
  *
@@ -50,15 +198,16 @@ __find_cblk_schurfcol( symbol_matrix_t *symbptr )
  *
  * @param[inout] symbptr
  *          The symbol structure to initialize.                                             *
+ * @param[in] ordeptr
+ *          The ordering structure providing rangtab for supernode
+ *          and the permutation tab. This parameter must be given before
+ *          its expension.
+ *
  *******************************************************************************/
 void
-pastixSymbolExtend(       symbol_matrix_t *symbptr )
+pastixSymbolExtend(       symbol_matrix_t *symbptr,
+                    const pastix_order_t  *ordeptr )
 {
-    /*Update block row and column indexes*/
-    pastix_int_t   col, row, cblknbr, bloknbr, dof;
-    symbol_cblk_t *cblk, *schurcblk;
-    symbol_blok_t *blok;
-
     if ( symbptr == NULL ) {
         printf( "Warning : pastixSymbolExtend : symbmatrix is not initialised\n" );
         return;
@@ -66,39 +215,11 @@ pastixSymbolExtend(       symbol_matrix_t *symbptr )
 
     pastixSymbolBase( symbptr, 0 );
 
-    dof     = symbptr->dof;
-    cblknbr = symbptr->cblknbr;
-    cblk    = symbptr->cblktab;
-    /* Update cblks first */
-    for (col = 0; col <= cblknbr; ++col)
-    {
-        if (dof >= 1) {
-            cblk[col].fcolnum *= dof;
-            cblk[col].lcolnum *= dof;
-        }
-        else {
-        }
-    }
-
-    bloknbr = symbptr->bloknbr;
-    blok    = symbptr->bloktab;
-    /* Update blocks then */
-    for (row = 0; row < bloknbr; ++row)
-    {
-        if (dof >= 1) {
-            blok[row].frownum *= dof;
-            blok[row].lrownum *= dof;
-        }
-        else {
-        }
-    }
-
-    if ( dof >= 1) {
-        symbptr->nodenbr   *= dof;
-        symbptr->schurfcol *= dof;
+    if ( symbptr->dof >= 1 ) {
+        __extend_dof_cte( symbptr, ordeptr );
     }
     else {
-        schurcblk = __find_cblk_schurfcol( symbptr );
+        __extend_dof_not_cte( symbptr, ordeptr );
     }
 
     symbptr->dof = 1;
