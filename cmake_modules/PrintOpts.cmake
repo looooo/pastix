b4ebb88dfc91a8731c3de296a3a40d323fb7e40f
tdelarue <tony.delarue@inria.fr>
2019-11-22 11:45:28 +0100

    Resolving threads

--- cmake_modules/PrintOpts.cmake
+++ cmake_modules/PrintOpts.cmake
@@ -1,7 +1,7 @@
 ###
 #  @file PrintOpts.cmake
 #
-#  @copyright 2013-2019 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
+#  @copyright 2019-2019 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
 #                       Univ. Bordeaux. All rights reserved.
 #
 #  @version 6.0.3
@@ -11,12 +11,12 @@
 ###
 
 set(dep_message "\nConfiguration of Pastix:\n"
-        "       PASTIX_VERSION ......: ${PASTIX_VERSION_MAJOR}.${PASTIX_VERSION_MINOR}.${PASTIX_VERSION_MICRO}\n"
-        "       BUILDNAME ...........: ${BUILDNAME}\n"
-        "       SITE ................: ${SITE}\n"
-        "\n"
-        "       Compiler: C .........: ${CMAKE_C_COMPILER} (${CMAKE_C_COMPILER_ID})\n"
-        "       Compiler: Fortran ...: ${CMAKE_Fortran_COMPILER} (${CMAKE_Fortran_COMPILER_ID})\n")
+"       PASTIX_VERSION ......: ${PASTIX_VERSION_MAJOR}.${PASTIX_VERSION_MINOR}.${PASTIX_VERSION_MICRO}\n"
+"       BUILDNAME ...........: ${BUILDNAME}\n"
+"       SITE ................: ${SITE}\n"
+"\n"
+"       Compiler: C .........: ${CMAKE_C_COMPILER} (${CMAKE_C_COMPILER_ID})\n"
+"       Compiler: Fortran ...: ${CMAKE_Fortran_COMPILER} (${CMAKE_Fortran_COMPILER_ID})\n")
 if(PASTIX_WITH_MPI)
   set(dep_message "${dep_message}"
   "       Compiler: MPI .......: ${MPI_C_COMPILER}\n"
@@ -49,13 +49,12 @@ set(dep_message "${dep_message}"
 "       LAPACKE .............: ${LAPACKE_FOUND}\n"
 "       HWLOC ...............: ${HWLOC_FOUND}\n"
 "\n"
-"       Blend Trace .........: ${PASTIX_BLEND_GENTRACE}\n"
 "       Trace ...............: ${PASTIX_WITH_EZTRACE}\n"
 "\n"
 "       Binaries to build\n"
 "       documentation .......: ${BUILD_DOCUMENTATION}\n"
 "       testing .............: ${BUILD_TESTING}\n"
-"       Precisions ..........: ${PASTIX_PRECISIONS}\n"
+"       precisions ..........: ${PASTIX_PRECISIONS}\n"
 "\n"
 "       INSTALL_PREFIX ......: ${CMAKE_INSTALL_PREFIX}\n\n")
 
