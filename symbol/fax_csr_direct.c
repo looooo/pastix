0c9eedbb7c5266385b3d39027f90e65d695989bb
nbredel <nbredel@gmail.com>
2021-06-16 16:37:16 +0200

    Change a int to pastix_int_t

--- symbol/fax_csr_direct.c
+++ symbol/fax_csr_direct.c
@@ -116,7 +116,7 @@ faxCSRFactDirect( const fax_csr_t *graphA, const pastix_order_t *order, fax_csr_
     /* Check that all terms of A are in the pattern */
     {
         pastix_int_t ind;
-        int j;
+        pastix_int_t j;
         for ( k = 0; k < cblknbr; k++ ) {
             /* Put the diagonal elements (A does not contains them) */
             for ( i = rangtab[k]; i < rangtab[k + 1]; i++ ) {
