90193cb42bb110c2dec86e20053dfc0eb15e7f1d
BREDEL Nolan <nolan.bredel@inria.fr>
2021-07-06 14:49:20 +0000

    Communication matrix between nodes

---
+++ blend/solver_comm_matrix.c
@@ -0,0 +1,104 @@
+/**
+ *
+ * @file solver_com_matrix.c
+ *
+ * PaStiX communication matrix handler.
+ *
+ * @copyright 1998-2021 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
+ *                      Univ. Bordeaux. All rights reserved.
+ *
+ * @version 6.2.1
+ * @author Nolan Bredel
+ * @date 2021-07-02
+ *
+ **/
+#include "common/common.h"
+#include "blend/solver_comm_matrix.h"
+
+#if !defined(PASTIX_COMMUNICATION_MATRIX)
+#error "PASTIX_COMMUNICATION_MATRIX option is disabled"
+#endif
+
+/**
+ *******************************************************************************
+ *
+ * @brief Initialize the communication matrix.
+ *
+ *******************************************************************************
+ *
+ * @param[inout] solvmtx
+ *          The solver structure to which to attach the communication vector.
+ *
+ *******************************************************************************/
+void
+solverComMatrixInit( SolverMatrix *solvmtx )
+{
+    solvmtx->com_vector = calloc( solvmtx->clustnbr, sizeof( size_t ) );
+}
+
+/**
+ *******************************************************************************
+ *
+ * @brief Free the communication matrix.
+ *
+ *******************************************************************************
+ *
+ * @param[inout] solvmtx
+ *          The solver structure to which to free the communication vector.
+ *
+ *******************************************************************************/
+void
+solverComMatrixExit( SolverMatrix *solvmtx )
+{
+    free( solvmtx->com_vector );
+    solvmtx->com_vector = NULL;
+}
+
+/**
+ *******************************************************************************
+ *
+ * @brief Gather the volume of communication and save it as a csv.
+ *
+ *******************************************************************************
+ *
+ * @param[inout] solvmtx
+ *          The solver structure with the communication vector to gather.
+ *
+ *******************************************************************************/
+void
+solverComMatrixGather( SolverMatrix *solvmtx )
+{
+    size_t *com_matrix = NULL;
+    int     src;
+    FILE *  file;
+
+    MPI_Comm_rank( solvmtx->solv_comm, &src );
+    if ( src == 0 ) {
+        com_matrix = malloc( solvmtx->clustnbr * solvmtx->clustnbr * sizeof( size_t ) );
+    }
+
+    MPI_Gather( solvmtx->com_vector, solvmtx->clustnbr, MPI_UNSIGNED_LONG,
+                com_matrix,          solvmtx->clustnbr, MPI_UNSIGNED_LONG,
+                0, solvmtx->solv_comm );
+
+    if ( src == 0 )
+    {
+        file = pastix_fopenw( ".", "com_matrix.csv", "w" );
+
+        /* header */
+        int p;
+        for ( p = 0; p < solvmtx->clustnbr; p++ ) {
+            fprintf( file, "R%d, ", p );
+        }
+        fprintf( file, "\n" );
+
+        int i;
+        for ( i = 0; i < ( solvmtx->clustnbr * solvmtx->clustnbr ); i++ ) {
+            fprintf( file, "%lu, ", com_matrix[i] );
+            if ( ( ( i + 1 ) % solvmtx->clustnbr ) == 0 ) {
+                fprintf( file, "\n" );
+            }
+        }
+    }
+    free( com_matrix );
+}
---
+++ blend/solver_comm_matrix.h
@@ -0,0 +1,66 @@
+/**
+ *
+ * @file solver_com_matrix.h
+ *
+ * PaStiX communication matrix handler.
+ *
+ * @copyright 2021-2021 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
+ *                      Univ. Bordeaux. All rights reserved.
+ *
+ * @version 6.2.1
+ * @author Nolan Bredel
+ * @date 2021-07-02
+ *
+ **/
+#ifndef _solver_comm_matrix_h_
+#define _solver_comm_matrix_h_
+
+#include "common/common.h"
+#include "blend/solver.h"
+
+#if defined(PASTIX_COMMUNICATION_MATRIX)
+void solverComMatrixInit( SolverMatrix *solvmtx );
+void solverComMatrixExit( SolverMatrix *solvmtx );
+void solverComMatrixGather( SolverMatrix *solvmtx );
+
+/**
+ *******************************************************************************
+ *
+ * @brief Add the size of a communication
+ *
+ *******************************************************************************
+ *
+ * @param[inout] solvmtx
+ *          The solver matrix structure.
+ *
+ * @param[in] dest
+ *          The destination node where the data goes.
+ *
+ * @param[in] size
+ *          Size of the communication.
+ *
+ *******************************************************************************/
+static inline void
+solverCommMatrixAdd( SolverMatrix *solvmtx, int dest, size_t size )
+{
+    solvmtx->com_vector[dest] += size;
+}
+
+#else
+static inline void
+solverComMatrixInit( __attribute__((unused)) SolverMatrix *solvmtx ) {}
+
+static inline void
+solverComMatrixExit( __attribute__((unused)) SolverMatrix *solvmtx ) {}
+
+static inline void
+solverComMatrixGather( __attribute__((unused)) SolverMatrix *solvmtx ) {}
+
+static inline void
+solverCommMatrixAdd( __attribute__((unused)) SolverMatrix *solvmtx,
+                     __attribute__((unused)) int           dest,
+                     __attribute__((unused)) size_t        size )
+{}
+#endif
+
+#endif /* _solver_comm_matrix_h_ */
