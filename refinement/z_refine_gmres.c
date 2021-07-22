c21ba68a36580f4fca6f4dd977aaf73a91bda181
Nolan Bredel <nbredel@enseirb-matmeca.fr>
2021-07-22 16:06:26 +0200

    Fix refinement display with MPI

--- refinement/z_refine_gmres.c
+++ refinement/z_refine_gmres.c
@@ -265,7 +265,8 @@ pastix_int_t z_gmres_smp(pastix_data_t *pastix_data, void *x, void *b)
 
             clockStop((refine_clk));
             t3 = clockGet();
-            if ( pastix_data->iparm[IPARM_VERBOSE] > PastixVerboseNot ) {
+            if ( ( pastix_data->iparm[IPARM_VERBOSE] > PastixVerboseNot ) &&
+                 ( pastix_data->procnum == 0 ) ) {
                 solver.output_oneiter( t0, t3, resid_b, iters );
 
 #if defined(PASTIX_DEBUG_GMRES)
--- refinement/z_refine_grad.c
+++ refinement/z_refine_grad.c
@@ -144,7 +144,8 @@ pastix_int_t z_grad_smp(pastix_data_t *pastix_data, void *x, void *b)
 
         clockStop((refine_clk));
         t3 = clockGet();
-        if ( pastix_data->iparm[IPARM_VERBOSE] > PastixVerboseNot ) {
+        if ( ( pastix_data->iparm[IPARM_VERBOSE] > PastixVerboseNot ) &&
+             ( pastix_data->procnum == 0 ) ) {
             solver.output_oneiter( t0, t3, resid_b, nb_iter );
         }
         t0 = t3;
