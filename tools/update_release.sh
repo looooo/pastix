5aca71fb20436f2dd2c6e47cb3499a8d6cf800a6
Mathieu Faverge <mathieu.faverge@inria.fr>
2023-07-25 00:33:09 +0200

    Add some info about the steps for the release

--- tools/update_release.sh
+++ tools/update_release.sh
@@ -4,30 +4,66 @@
 #  @copyright 2016-2023 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
 #                       Univ. Bordeaux. All rights reserved.
 #
-#  @version 6.3.0
+#  @version 6.3.1
 #  @author Mathieu Faverge
-#  @date 2023-08-01
+#  @date 2023-10-25
 #
 #!/usr/bin/env sh
 
-tag=$1
-#tag=v6.2.0
+# subset of commit to use to select the files tp work with
+subset="diff --name-only HEAD~1"
+
+release="no"
+
 majorversion=6
 minorversion=3
-microversion=0
+microversion=1
 
 version="$majorversion.$minorversion.$microversion"
 
-#for i in $( git diff v6.0.2 --name-only ); do if [ -f $i ]; then sed -i 's/@version [0-9].[0-9].[0-9]/@version 6.3.0/' $i; fi; done
-if [ ! -z "$tag" ]
-then
-    #fileslist=$( git diff $tag --name-only )
-    fileslist=$( git diff --name-only HEAD~1 )
-else
-    fileslist=$( git ls-files )
-fi
+remotelogin=faverge
+locallogin=mathieu
 
-fulllist=$( git ls-files )
+while [ $# -gt 0 ]
+do
+    case $1 in
+	--major )
+	    shift
+	    majorversion="$1"
+	    ;;
+	--minor )
+	    shift
+	    minorversion="$1"
+	    ;;
+	--micro )
+	    shift
+	    microversion="$1"
+	    ;;
+        --all )
+            subset="ls-files"
+            ;;
+        --release )
+            subset="ls-files"
+            release="yes"
+            ;;
+        --rlogin )
+	    shift
+            remotelogin="$1"
+            ;;
+        --llogin )
+	    shift
+            locallogin="$1"
+            ;;
+	*)
+            # Let's consider that anything else is a tag or a list of commit to study
+	    subset="diff --name-only $1"
+	    ;;
+    esac
+    shift
+done
+
+# Let's get the list of files to update
+fileslist=$( git $subset )
 
 #
 # Steps to update header information before doing the release
@@ -59,6 +95,14 @@ do
 done
 
 #
+# 3b) Update the author list
+#
+if [ $release = "no" ]
+then
+    ./tools/check_authors.sh $fileslist
+fi
+
+#
 # 4) Update the release number
 #
 for f in $fileslist
@@ -74,19 +118,24 @@ done
 #
 # 5a) Update manually the version number in CMakeLists.txt
 #
-sed -i "s/set( PASTIX_VERSION_MAJOR [0-9] )/set( PASTIX_VERSION_MAJOR $majorversion )/" CMakeLists.txt
-sed -i "s/set( PASTIX_VERSION_MINOR [0-9] )/set( PASTIX_VERSION_MINOR $minorversion )/" CMakeLists.txt
-sed -i "s/set( PASTIX_VERSION_MICRO [0-9][0-9]* )/set( PASTIX_VERSION_MICRO $microversion )/" CMakeLists.txt
+if [ $release = "yes" ]
+then
+    sed -i "s/set( PASTIX_VERSION_MAJOR [0-9] )/set( PASTIX_VERSION_MAJOR $majorversion )/" CMakeLists.txt
+    sed -i "s/set( PASTIX_VERSION_MINOR [0-9] )/set( PASTIX_VERSION_MINOR $minorversion )/" CMakeLists.txt
+    sed -i "s/set( PASTIX_VERSION_MICRO [0-9][0-9]* )/set( PASTIX_VERSION_MICRO $microversion )/" CMakeLists.txt
 
-#
-# 5b) Update manually the analysis.sh script
-#
-sed -i "s/sonar.projectVersion=.*$/sonar.projectVersion=$version/" tools/analysis.sh
+    #
+    # 5b) Update manually the analysis.sh script
+    #
+    analysismicro=$(( microversion + 1 ))
+    analysisversion="$majorversion.$minorversion.$analysismicro"
+    sed -i "s/sonar.projectVersion=.*$/sonar.projectVersion=$analysisversion/" tools/analysis.sh
+fi
 
 #
 # 6) If necessary, update the copyright information
 #
-for f in $fulllist
+for f in $fileslist
 do
     if [ ! -f $f ]
     then
@@ -104,5 +153,22 @@ do
 done
 
 #
-# 7) Update homebrew formula (only after release)
+# 7) Add the release to files.inria.fr/pastix/releases
 #
+
+if [ $release = "yes" ]
+then
+    # Cf https://doc-si.inria.fr/display/SU/Espace+web# to mount the remote filesystem
+    # Here are the linux commands (Using VPN)
+    #
+    # sudo apt-get install davfs2
+    mkdir -p /tmp/webdav-pastix
+    sudo mount.davfs -o username=$remotelogin,user,noauto,uid=$locallogin https://files.inria.fr:8443/pastix /tmp/webdav-pastix
+    cd /tmp/webdav-pastix/releases
+    tree -rv -H https://files.inria.fr/pastix/releases >! index.html
+fi
+
+#
+# 8) Update homebrew formula (only after release)
+#
+
