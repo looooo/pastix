d660c6e3414e1a20175b3c9da6778f39ee50cef6
Mathieu Faverge <mathieu.faverge@inria.fr>
2023-12-04 14:51:41 +0100

    starpu/codelet_fanin_init.c: Add a rhs_init codelet.

--- sopalin/starpu/codelet_fanin_init.c
+++ sopalin/starpu/codelet_fanin_init.c
@@ -8,9 +8,10 @@
  * @copyright 2016-2023 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
  *                      Univ. Bordeaux. All rights reserved.
  *
- * @version 6.3.1
+ * @version 6.4.0
  * @author Alycia Lisito
- * @date 2023-12-01
+ * @author Mathieu Faverge
+ * @date 2023-12-18
  *
  * @addtogroup pastix_starpu
  * @{
@@ -63,6 +64,49 @@ struct starpu_codelet cl_fanin_init_cpu = {
     .nbuffers  = 1,
     .modes[0]  = STARPU_W,
     .name      = "fanin_init",
+};
+
+#if !defined(PASTIX_STARPU_SIMULATION)
+/**
+ *******************************************************************************
+ *
+ * @brief StarPU CPU implementation
+ *
+ *******************************************************************************
+ *
+ * @param[in] descr
+ *          TODO
+ *
+ * @param[in] cl_arg
+ *          TODO
+ *
+ *******************************************************************************/
+static void
+fct_rhs_init_cpu( void *descr[], void *cl_arg )
+{
+    void   *A    = (void*)STARPU_MATRIX_GET_PTR( descr[0] );
+    size_t  size = ((struct starpu_matrix_interface *)descr[0])->allocsize;
+
+    assert( size > 0 );
+    memset( A, 0, size );
+
+    (void)cl_arg;
+}
+#endif /* !defined(PASTIX_STARPU_SIMULATION) */
+
+/**
+ * @brief Main structure for all tasks of rhs_init type
+ */
+struct starpu_codelet cl_rhs_init_cpu = {
+    .where     = STARPU_CPU,
+#if !defined(PASTIX_STARPU_SIMULATION)
+    .cpu_funcs[0] = fct_rhs_init_cpu,
+#else
+    .cpu_funcs[0] = (starpu_cpu_func_t)1,
+#endif
+    .nbuffers  = 1,
+    .modes[0]  = STARPU_W,
+    .name      = "rhs_init",
 };
 
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
